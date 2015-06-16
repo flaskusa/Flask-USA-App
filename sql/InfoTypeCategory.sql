8ec46ffdb2ca963915c9935c01989b061d6c58ab
admin <admin@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-06-16 03:23:05 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@276 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ sql/InfoTypeCategory.sql
@@ -0,0 +1,14 @@
+insert into flaskevents_infotypecategory values(1,"General","");
+insert into flaskevents_infotypecategory values(2,"Tradition","");
+insert into flaskevents_infotypecategory values(3,"Weather","");
+insert into flaskevents_infotypecategory values(4,"Parking","");
+insert into flaskevents_infotypecategory values(5,"Traffic","");
+insert into flaskevents_infotypecategory values(6,"Food","");
+insert into flaskevents_infotypecategory values(7,"Liquor","");
+insert into flaskevents_infotypecategory values(8,"Advertisements","");
+insert into flaskevents_infotypecategory values(9,"Hospitals","");
+insert into flaskevents_infotypecategory values(10,"First Aid","");
+insert into flaskevents_infotypecategory values(11,"Police Stations","");
+insert into flaskevents_infotypecategory values(12,"Fire Stations","");
+
+
