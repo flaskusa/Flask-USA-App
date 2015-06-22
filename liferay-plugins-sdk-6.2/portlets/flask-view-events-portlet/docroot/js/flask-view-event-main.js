756c5bd9d504523c7b152dbaed5f91ecfa8a5adf
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-06-22 14:23:43 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@319 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-event-main.js
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-event-main.js
@@ -5,6 +5,7 @@ function fnLoadList() {
  var request = new Request();
  request.sendGETRequest(SERVICE_ENDPOINTS.GET_ALL_EVENTS, params,
    function(data) {
+	 console.log(data);
     if (data.responseStatus == 1) {
      alllist = data.responseJson;
      fnlist(alllist);
@@ -15,22 +16,18 @@ function fnLoadList() {
 }
 
 function fnlist(tdata) {
-	
  var source = {
   localdata : tdata,
   datatype : "json",
   datafields : DATA_SOURCE.GET_FLASK_EVENT_LIST,
-
  };
  var dataAdapter = new $.jqx.dataAdapter(source);
-
     $('#listbox').on('click', function (event) {
-    	
     		$('#one').hide();
     		$('#two').show();
-
+    		fnLoadSlider();
+    		//$('#two').show();
     });
-
     // Create jqxListBox
     $('#listbox').jqxListBox({ source: dataAdapter, displayMember: "Eventname", itemHeight: 70, scrollBarSize: 0, height: '100%', width: '100%', 
         renderer: function (index, label, value) {
@@ -44,14 +41,44 @@ function fnlist(tdata) {
     $('#listbox').jqxListBox('refresh');
 }
 
-$(document).ready(function() 
-		
-{
-	$('#listbox').jqxListBox('refresh');
+$(document).ready(function(){
+		$('#listbox').jqxListBox('refresh');
 		fnLoadList();
-		 $(".cssback").click(function(){
-			 
+		$(".cssback").click(function(){
 			$('#one').show();
 			$('#two').hide();
 		});
 });
+
+function fnLoadSlider(){
+	var arr = [21674,21675,21682];
+	for(var iCount=0;iCount < arr.length;++iCount){
+		var params = {repositoryId:20181,folderId:arr[iCount]};
+		var request = new Request();
+		request.sendGETRequest(SERVICE_ENDPOINTS.GET_ALL_IMAGES, params,
+		function(data) {
+			if (data.responseStatus == 1) {
+				fnRenderSlider(data.responseJson);
+			} else {
+				alert("MESSAGES.ERROR_REGISTER_USER");
+			}
+		});		
+	}
+}
+
+function fnRenderSlider(objJson){
+	var tempHTML="";
+	var url="";
+	//ws_images
+	
+	var wowContainer = $('<div/>').attr("class","wowslider-container");
+	var wsimages = $('<div/>').attr("class","ws_images");
+	var list = $('<ul/>').appendTo($(wsimages));
+	for(iCount=0;iCount<objJson.length;++iCount){
+		url = "/documents/"+objJson[iCount].repositoryId+"/"+objJson[iCount].folderId+"/"+objJson[iCount].title;
+		list.append('<li><img src='+url+' alt='+objJson[iCount].userName+' title='+objJson[iCount].userName+'/></li>');
+	}
+	$(wsimages).appendTo($(wowContainer));
+	$(wowContainer).appendTo($("#wowContainer"));
+	$(wowContainer).wowSlider();
+}
