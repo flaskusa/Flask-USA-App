61dc966a1718b0878cdbf29c1e82d06a5f121036
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-07-15 14:10:12 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@530 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-event-main.js
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-event-main.js
@@ -24,25 +24,36 @@ function fnlist(tdata) {
 	 function formatUnixToTime(tdate)
 		{
 			var date = new Date(tdate);
-			// hours part from the timestamp
 			var hours = date.getHours();
-			// minutes part from the timestamp
 			var minutes = "0" + date.getMinutes();
 
 			var ampm = hours >= 12 ? 'PM' : 'AM';
 			hours = hours % 12;
-			// will display time in 10:30 PM format
 			return hours + ':' + minutes.substr(-2) + ' ' + ampm;
 		}
 	 
-	 var divRow = $('<table/>',{'class':'tableSize'});
+	 var divRow = $('#placeholder');
 	 for(var i in tdata)
 		{
 		    var st = formatUnixToTime(tdata[i].startTime);
-		    var divRowItem = $('<div/>',{'title':tdata[i].eventName, 'class':'rowHeight'});
-		 	var tempHTML = '<tr><td style="width:20%;"><div class="imgdiv"><img src="'+ tdata[i].eventImagePath + '" ></div><div class="lbldiv"><label class="control-label-color">' + tdata[i].eventName + '</label><label class="control-label-nocolor">'+ st + ' at ' + tdata[i].venueId + '</label></div></td></tr>';
-		 	$(divRowItem).html(tempHTML);
+		    var divRowItem = $('<div/>',{'class':'row-fluid','title':tdata[i].eventName,'data-id':tdata[i].eventId});
+		    var divCol3 = $('<div/>',{'class':'span3'});
+		    var divCol9 = $('<div/>',{'class':'span9'});
+		    var divCol9_lbl = $('<div/>',{'class':'lbldiv'});
+		    var eventName_lbl = $('<label/>',{'class':'control-label-color'});
+		    $(eventName_lbl).html(tdata[i].eventName);
+		    var venue_lbl = $('<label/>',{'class':'control-label-nocolor'});
+		    $(venue_lbl).html(st + ' at ');
+		    $(eventName_lbl).appendTo($(divCol9_lbl));
+		    $(venue_lbl).appendTo($(divCol9_lbl));
+		    $(divCol9_lbl).appendTo($(divCol9));
+		 	$(divCol3).appendTo($(divRowItem));
+		 	$(divCol9).appendTo($(divRowItem));
 		 	$(divRowItem).appendTo($(divRow));
+		 	
+		    fnGetVenueName(venue_lbl,tdata[i].venueId);
+		 	fnShowEventLogo($("#repositoryId").val(),tdata[i].eventId,divCol3)
+		 	
 		 	$(divRowItem).click(function(){
 		 		$('#one').hide();
 		 		$("#spinningSquaresG").show(); 
@@ -51,25 +62,21 @@ function fnlist(tdata) {
 					$("#spinningSquaresG").hide();	
 				 	$('#two').show();	
 		 		},3);
-		 		fnRenderEvents($("#repositoryId").val(),0,'Events',$(this).attr('title'));
-		 		
+		 		//fnRenderEvents($("#repositoryId").val(),0,'Events',$(this).attr('title'));
+		 		fnShowSlider($(this).attr('data-id'),"",2);
+		 		fnShowSlider($(this).attr('data-id'),"",3);
+		 		fnShowSlider($(this).attr('data-id'),"",4);
 		 	});
 	    }
 	 	$(divRow).appendTo($("#placeholder"));
 }
 
 $(document).ready(function(){
-	
 	fnLoadList();
 	$(".cssback").click(function(){
 		$('#one').show();
 		$('#two').hide();
 	});
-	 /*var targetUrl = $(this).attr('#one');
-	  window.history.pushState({url: "" + targetUrl + ""}, null, targetUrl);
-	  window.onpopstate = function(e) {
-		    window.history.go(e.state ? e.state.url : null);
-		};*/
 });
 
 function fnRenderEvents(repositoryId,parentFolderId,folderName,eventName){
@@ -123,46 +130,207 @@ function fnRenderSliders(repositoryId,parentFolderId,folderName,Count){
 		return folderId;
 }
 
-function displayImages(repositoryId, folderId, ws_images, wowslider_container)
+function displayImages(repositoryId, folderId, ws_images, wowslider_container,ulObj)
 {
 	var flaskRequest = new Request();
 	params= {'repositoryId': repositoryId, 'folderId': folderId};
 	flaskRequest.sendGETRequest(SERVICE_ENDPOINTS.GET_ALL_IMAGES, params, 
-		
     function(obj) {
 	  obj = obj.responseJson;
       if(typeof obj=="object" && obj.length > 0)
       {
 			var temp_html;
+			temp_html = "";
 			var imageUrl;
 			var uploadedby;
-			temp_html="<ul>";
 			for(var i=0;i<obj.length;i++)
 			{
+				var liObj = $("<li/>");				
 				imageUrl = "/documents/"+repositoryId+"/"+folderId+"/"+obj[i].title;
 				uploadedby = "uploaded by " + obj[i].userName;
-				temp_html=temp_html+"<li><img src="+imageUrl+" alt='"+uploadedby+"' title='"+uploadedby+"' id='wows1_0'/></li>";
+				$(liObj).html("<img src="+imageUrl+" alt='"+uploadedby+"' title='"+uploadedby+"' id='wows1_0'/>");
+				$(liObj).appendTo(ulObj);
 			}
-			temp_html=temp_html+"</ul>";
-			$(ws_images).html(temp_html);
-			$(wowslider_container).wowSlider();
-	
       }
       else
       {
-    	  var temp_html;
+    	  	var temp_html;
 			var imageUrl;
-			var uploadedby;
-			temp_html="<ul>";
-				imageUrl = "/flask-view-events-portlet/img/NoData.png";
-				uploadedby = "No data";
-				temp_html=temp_html+"<li><img src='"+imageUrl+"' alt='"+uploadedby+"' title='"+uploadedby+"' id='wows1_0'/></li>";
-			temp_html=temp_html+"</ul>";
-			$(ws_images).html(temp_html);
-			$(wowslider_container).wowSlider();
-		
+			imageUrl = "/flask-view-events-portlet/img/NoData.png";
+			var liObj = $("<li/>");				
+			uploadedby = "No data";
+			$(liObj).html("<img src="+imageUrl+" alt='"+uploadedby+"' title='"+uploadedby+"' id='wows1_0'/>");
+			$(liObj).appendTo(ulObj);
       }
-      
+      $(ulObj).appendTo($(ws_images))      
+      $(wowslider_container).wowSlider();
  });
 }
 
+function fnShowEventLogo(_repositoryId,_eventId,_container){
+	///LOGO START
+	var LogoURL = "";
+	var flaskRequest = new Request();
+	params= {'repositoryId': _repositoryId, 'parentFolderId': 0, 'name': 'Event'};
+	flaskRequest.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDER , params, 
+		function (data){
+			folderName = 'Event-'+_eventId;
+			folderId = data.responseJson.folderId;
+			var flaskRequestChild = new Request();
+			paramsChild= {'repositoryId': _repositoryId, 'parentFolderId': folderId, 'name': folderName};
+			flaskRequestChild.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDER, paramsChild, 
+				function (data){
+					var _folderId = data.responseJson.folderId;
+					var flaskRequestChild0 = new Request();
+					paramsChild0= {'groupId': _repositoryId, 'folderId':_folderId, 'title': 'EventLogo'};
+					flaskRequestChild0.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FILE_BY_TITLE , paramsChild0, 
+						function (data){
+							LogoURL = "/documents/"+_repositoryId+"/"+_folderId+"/EventLogo";
+						    var objdiv = $('<div/>',{'class':'eventLogo','style':'background-image:url('+LogoURL+')','data-folderId':_folderId,'data-title':'EventLogo'});
+							$(_container).append(objdiv);
+						} ,
+						function (data){
+							//console.log('test-0');
+					});				
+				} ,
+				function (data){
+					//console.log('test-1');
+			});
+		} ,
+		function (data){
+			//console.log('test-2');
+	});
+	//LOGO END
+}
+
+function fnGetVenueName(_container,_venueId){
+	var flaskRequest = new Request();
+	flaskRequest.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_ALL_VENUES, "", 
+		function (data){
+			var dataJson = data.responseJson;
+			var returnedData = $.grep(dataJson, function(element, index){
+		    	return element.venueId == _venueId;
+		    });
+			var dataFilterJson =returnedData; 
+		    var t = $("<span/>");
+		    $(t).html(dataFilterJson[0].venueName);
+		    $(t).appendTo($(_container));
+	},
+		function (data){
+			console.log(data);
+	});
+}
+
+
+function fnShowSlider(_eventId,_divObj,_infoTypeId){
+	console.log(_eventId);
+	var ws_images = "#ws_images"+_infoTypeId;
+	var wowslider_container = "#wowslider-container"+_infoTypeId;
+	$(ws_images).html("");
+	$(_divObj).html("");
+	var repositoryId = $("#repositoryId").val();
+	var eventId = _eventId;
+	var infoTypeId = _infoTypeId;
+	var flaskRequest = new Request();
+	params= {'repositoryId': repositoryId, 'parentFolderId': 0, 'name': 'Event'};
+	flaskRequest.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDER , params, 
+		function (data){
+			if(data.responseJson.exception!=""){
+				fnBlankSlide(ws_images,wowslider_container);
+			}
+			folderName = 'Event-'+eventId;
+			var flaskRequestChild = new Request();
+			paramsChild= {'repositoryId': repositoryId, 'parentFolderId': data.responseJson.folderId, 'name': folderName};
+			flaskRequestChild.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDER , paramsChild, 
+				function (data){
+					if(data.responseJson.exception!=""){
+						fnBlankSlide(ws_images,wowslider_container);
+					}
+					var flaskRequestChild1 = new Request();
+					paramsChild1= {'repositoryId': repositoryId, 'parentFolderId': data.responseJson.folderId, 'name': infoTypeId};
+					flaskRequestChild1.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDER , paramsChild1, 
+						function (data){
+							if(data.responseJson.exception!=""){
+								fnBlankSlide(ws_images,wowslider_container);
+							}
+							var flaskRequestChild2 = new Request();
+							paramsChild2= {'repositoryId': repositoryId, 'parentFolderId': data.responseJson.folderId};
+							flaskRequestChild2.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDERS , paramsChild2,
+								function (data){
+									if(data.responseJson.exception!=""){
+										fnBlankSlide(ws_images,wowslider_container);
+									}
+									var dataJson = data.responseJson;
+									for(var iCount=0;iCount<dataJson.length;iCount++){
+										folderId = dataJson[iCount].folderId;
+										var flaskRequestChild3 = new Request();
+										paramsChild2= {'repositoryId': repositoryId, 'parentFolderId': folderId};
+										flaskRequestChild3.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FOLDERS , paramsChild2, 
+											function (data){
+												if(data.responseJson.exception!=""){
+													fnBlankSlide(ws_images,wowslider_container);
+												}
+												var _dataJson = data.responseJson;
+												var tempHtml;
+												var ulObj = $("<ul/>");
+												for(var _iCount=0;_iCount<_dataJson.length;_iCount++){
+													displayImages(repositoryId, _dataJson[_iCount].folderId, ws_images, wowslider_container,ulObj);
+												}
+											} ,
+										function (data){
+											fnBlankSlide(ws_images,wowslider_container)
+										});
+									}
+								} ,
+								function (data){
+									fnBlankSlide(ws_images,wowslider_container)
+								});
+						} ,
+						function (data){
+							fnBlankSlide(ws_images,wowslider_container)
+						});
+
+				} ,
+				function (data){
+					fnBlankSlide(ws_images,wowslider_container)
+				});
+		} ,
+		function (data){
+			fnBlankSlide(ws_images,wowslider_container)
+		});
+}
+
+function fnRenderSlider(folderId,_divObj){
+	var flaskRequest = new Request();
+	params= {'repositoryId': $("#repositoryId").val(), 'folderId': folderId};
+	console.log(params);
+	flaskRequest.sendGETRequest(_eventModel.SERVICE_ENDPOINTS.GET_FILES , params, 
+		function (data){
+			console.log(data);
+			if(typeof data=="object"){
+				console.log(data);
+				var iSelected = false;
+		    	for(var iCount=0;iCount<data.length;iCount++){
+		    		var imgURL = '/documents/'+data[iCount].groupId+'/'+data[iCount].folderId+'/'+data[iCount].title;
+				    var objdiv = $('<div/>',{'class':'eventLogo','style':'background-image:url('+imgURL+')','data-fileEntryId':data[iCount].fileEntryId});
+				    $(objdiv).appendTo($(_divObj));
+		    	}
+		    }		
+		} ,
+		function (data){
+			console.log("Error in getting Folder: " + data );
+		});	
+}
+
+function fnBlankSlide(ws_images,wowslider_container){
+  	var temp_html;
+	var imageUrl;
+	imageUrl = "/flask-view-events-portlet/img/NoData.png";
+	var ulObj = $("<ul/>");
+	var liObj = $("<li/>");				
+	uploadedby = "No data";
+	$(liObj).html("<img src="+imageUrl+" alt='"+uploadedby+"' title='"+uploadedby+"' id='wows1_0'/>");
+	$(liObj).appendTo(ulObj);
+	$(ulObj).appendTo($(ws_images))      
+	$(wowslider_container).wowSlider();
+}
--- liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-eventmodel.js
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-eventmodel.js
@@ -3,17 +3,13 @@ var _eventModel = {};
 _eventModel.SERVICE_ENDPOINTS = {
 	API_PREFIX					:"/api/jsonws",
 	GET_EVENT 					: "/flask-rest-events-portlet.event/get-all-events",
-	ADD_EVENT 					: "/flask-rest-events-portlet.event/add-event",
-	ADD_EVENT_IMAGE				: "/flask-rest-events-portlet.event/add-event-image",
-	UPDATE_EVENT				: "/flask-rest-events-portlet.event/update-event",
-	DELETE_EVENT				: "/flask-rest-events-portlet.event/delete-event",
-	DELETE_EVENTS				: "/flask-rest-events-portlet.event/delete-events",
-	GET_EVENT_TYPES 			: "/flask-rest-events-portlet.eventtype/get-event-types",
-	GET_INFO_TYPES 				: "/flask-rest-events-portlet.infotype/get-info-types",
-	GET_INFO_CATEGORY 			: "/flask-rest-events-portlet.infotypecategory/get-info-type-categories",
-	GET_ALL_VENUES	 			: "/flask-rest-events-portlet.venue/get-all-venues",
-	GET_FOLDER					: "/dlapp/get-folder",
-	ADD_FOLDER					: "/dlapp/add-folder"
+	GET_ALL_VENUES	 			: "/api/jsonws/flask-rest-events-portlet.venue/get-all-venues",
+	GET_FOLDER					: "/api/jsonws/dlapp/get-folder",
+	GET_FOLDERS					: "/api/jsonws/dlapp/get-folders",
+	ADD_FOLDER					: "/api/jsonws/dlapp/add-folder",
+	GET_FOLDER					: "/api/jsonws/dlapp/get-folder",
+	GET_FILES					: "/api/jsonws/dlapp/get-file-entries",
+	GET_ALL_EVENT_DETAILS		: "/flask-rest-events-portlet.event/get-event-details"
 };
 
 _eventModel.DATA_MODEL= {
