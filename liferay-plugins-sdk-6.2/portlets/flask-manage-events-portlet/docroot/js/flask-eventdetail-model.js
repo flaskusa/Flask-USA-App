be49cdba56eef36c8c66a2d2a7d389f09c283618
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-08-07 16:37:14 +0000

    lat & lng in eventDetails
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@700 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-manage-events-portlet/docroot/js/flask-eventdetail-model.js
+++ liferay-plugins-sdk-6.2/portlets/flask-manage-events-portlet/docroot/js/flask-eventdetail-model.js
@@ -36,7 +36,7 @@ _eventDetailModel.DATA_MODEL= {
 				 { name: 'city', type: 'string'},
 				 { name: 'stateId', type: 'long' },
 				 { name: 'countryId', type: 'long' },
-				 { name: 'lattitude', type: 'string'},
+				 { name: 'latitude', type: 'string'},
 				 { name: 'longitude', type: 'string'},
 				 { name: 'phone', type: 'string'},
 				 { name: 'website', type: 'string'},
