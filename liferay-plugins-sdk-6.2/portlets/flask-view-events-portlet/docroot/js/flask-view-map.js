6ed5f087cdf45243a18f16bf017e2820e5c674c2
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-08-11 14:53:26 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@729 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-map.js
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-view-map.js
@@ -2,15 +2,11 @@ var geocoder;
 var map;
 var markers = Array();
 var infos = Array();
-
+var cur_location;
 function initialize() {
-    // prepare Geocoder
     geocoder = new google.maps.Geocoder();
-
-    // set initial position (New York)
     var myLatlng = new google.maps.LatLng(latitude,longitude);
-
-    var myOptions = { // default map options
+    var myOptions = {
         zoom: 16,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -18,8 +14,6 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
     findPlaces();
 }
-
-// clear overlays function
 function clearOverlays() {
     if (markers) {
         for (i in markers) {
@@ -29,8 +23,6 @@ function clearOverlays() {
         infos = [];
     }
 }
-
-// clear infos function
 function clearInfos() {
     if (infos) {
         for (i in infos) {
@@ -40,39 +32,50 @@ function clearInfos() {
         }
     }
 }
-
-// find custom places function
 function findPlaces() {
-
-    // prepare variables (filter)
     var radius = 500;
-
     var lat = latitude;
     var lng = longitude;
-    var cur_location = new google.maps.LatLng(lat, lng);
-
-    // prepare request to Places
+    cur_location = new google.maps.LatLng(lat, lng);
     var request = {
         location: cur_location,
         radius: radius,
         types: ['food', 'bar', 'store']
     };
-    // send request
     service = new google.maps.places.PlacesService(map);
     service.search(request, createMarkers);
+    
 }
-
-// create markers (from 'findPlaces' function)
 function createMarkers(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
     	clearOverlays();
-        // and create new markers by search result
+    	venue_mark = new google.maps.Marker({
+	        position: cur_location,
+	        map: map,
+	        animation:  google.maps.Animation.DROP,
+	        icon: '/flask-view-events-portlet/img/flag-marker.png',
+	    });
+    	markers.push(venue_mark);
+
+    	var infowindow = new google.maps.InfoWindow();
+    	google.maps.event.addListener(venue_mark, 'click', (function(venue_mark) {
+        	return function() {
+                clearInfos();
+                var content= '<div style="display: inline-flex;"><font style="color:#000; "><b>' + venueName + 
+                '</b><br /><br />' + venueAddr + '</font></div>';
+                infowindow.setContent(content);
+                infowindow.open(map,venue_mark);
+        	}
+        })(venue_mark));
+        infos.push(infowindow);
         for (var i = 0; i < results.length; i++) {
         	var mark;
             if(createMarker(results[i])){
+            	console.log(results[i].geometry.location);
             	mark = new google.maps.Marker({
 			        position: results[i].geometry.location,
 			        map: map,
+			        animation:  google.maps.Animation.DROP,
 			        title: results[i].name,
 			        icon: '/flask-view-events-portlet/img/flask-marker.png'
 			    });
@@ -85,13 +88,9 @@ function createMarkers(results, status) {
 			    });
             }
             markers.push(mark);
-           
             var infowindow = new google.maps.InfoWindow();
-
-            // add event handler to current marker
             google.maps.event.addListener(mark, 'click', (function(mark, i) {
             	return function() {
-            		console.log(results[i]);
 	                clearInfos();
 	                var content= '<div style="display: inline-flex;"><img src="' + results[i].icon + '" style="width:20%;"/><font style="color:#000; "><b>' + results[i].name + 
 	                '</b><br /><br />Vicinity: ' + results[i].vicinity + '</font></div>';
@@ -101,13 +100,10 @@ function createMarkers(results, status) {
             })(mark, i));
             infos.push(infowindow);        
         }
-        // prepare info window
     } else if (status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
         alert('Sorry, nothing is found');
     }
 }
-
-// creare single marker function
 function createMarker(obj) {
 	var isSubscribed = false;
 	var coords = new google.maps.LatLng(
@@ -116,10 +112,12 @@ function createMarker(obj) {
 		);
 	for(var i=0; i<lat_marker.length; i++)
 		{
-			console.log((parseFloat(lat_marker[i]).toFixed(2) == parseFloat(coords.lat()).toFixed(2))&&(parseFloat(lng_marker[i]).toFixed(2) == parseFloat(coords.lng()).toFixed(2))&&(obj.name==addr_name[i]));
-			if((parseFloat(lat_marker[i]).toFixed(2) == parseFloat(coords.lat()).toFixed(2))&&(parseFloat(lng_marker[i]).toFixed(2) == parseFloat(coords.lng()).toFixed(2))&&(obj.name==addr_name[i]))
+			if((parseFloat(lat_marker[i][0]).toFixed(2) == parseFloat(coords.lat()).toFixed(2))&&(parseFloat(lng_marker[i]).toFixed(2) == parseFloat(coords.lng()).toFixed(2))&&(obj.name==addr_name[i]))
 			{
-				isSubscribed = true;
+				if(lat_marker[i][1] == marker_infoType || lat_marker[i][1] == 1)
+					isSubscribed = true;
+				else
+					isSubscribed = false;
 			}
 		}
 	return isSubscribed;
