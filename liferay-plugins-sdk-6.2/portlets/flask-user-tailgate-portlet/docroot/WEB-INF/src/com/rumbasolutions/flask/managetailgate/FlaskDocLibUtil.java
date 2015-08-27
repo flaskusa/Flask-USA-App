e81cd706c198b993cc397b0c4d3d014947ab93d4
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-08-27 10:59:28 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@860 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ liferay-plugins-sdk-6.2/portlets/flask-user-tailgate-portlet/docroot/WEB-INF/src/com/rumbasolutions/flask/managetailgate/FlaskDocLibUtil.java
@@ -0,0 +1,227 @@
+package com.rumbasolutions.flask.managetailgate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.ResourceAction;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.ResourcePermission;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ResourceActionLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.service.RoleServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.bookmarks.NoSuchFolderException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+//import com.rumbasolutions.flask.model.TailgateInfo;
+//import com.rumbasolutions.flask.service.impl.EventServiceImpl;
+//import com.rumbasolutions.flask.service.TailgateInfoLocalService;
+//import com.rumbasolutions.flask.service.TailgateInfoLocalServiceUtil;
+
+public class FlaskDocLibUtil {
+	public static final String _tailgateRootFolder = "TailGate";
+		
+	public static String APP_NAME = "guest";
+	public static Role _guestRole =null;
+	
+	public static long _repositoryId = 0; 
+	public static long _eventId = 0;
+	//group id is used as repository id
+	public static long getFlaskRepositoryId() throws PortalException, SystemException{
+		if(_repositoryId==0){
+			long companyId = PortalUtil.getDefaultCompanyId();
+			Group group = GroupLocalServiceUtil.getGroup(companyId, APP_NAME);
+			_repositoryId = group.getGroupId();
+		}
+		return _repositoryId;	
+	}
+	
+	public static Role getGuestRole(){
+		try {
+			if(_guestRole == null){
+				_guestRole =RoleServiceUtil.getRole(PortalUtil.getDefaultCompanyId(), RoleConstants.GUEST);
+			}
+		}
+		catch (PortalException e) {
+			e.printStackTrace();
+		}
+		catch (SystemException e) {
+			e.printStackTrace();
+		}
+		return _guestRole;
+	}
+	
+	public static Folder createTailgateRootFolder(ServiceContext serviceContext) throws PortalException, SystemException{
+		long repositoryId = getFlaskRepositoryId();
+		long userId = serviceContext.getUserId();
+		Folder folder = getOrCreateFolder(_tailgateRootFolder, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, repositoryId, userId, serviceContext);
+		return folder;
+	}
+	
+	public static Folder createTailgateFolder(long eventId,ServiceContext serviceContext) throws PortalException, SystemException{
+		Folder folder = createTailgateRootFolder(serviceContext);
+		String eventFolderName = folder.getName()+"-"+eventId;
+		Folder eventFolder = getOrCreateFolder(eventFolderName, folder.getFolderId(), folder.getRepositoryId(), folder.getUserId(), serviceContext);
+		return eventFolder;
+	}
+	
+	public static Folder getTailgateRootFolder(ServiceContext serviceContext) throws PortalException, SystemException{
+		Folder folder=null;
+		long parentFolderId = DLFolderConstants.DEFAULT_PARENT_FOLDER_ID;
+		long repositoryId = getFlaskRepositoryId();
+		try {
+		  folder = DLAppLocalServiceUtil.getFolder( repositoryId, parentFolderId, _tailgateRootFolder);
+		} catch (final NoSuchFolderException e) {
+		  folder = DLAppLocalServiceUtil.addFolder(serviceContext.getUserId(),repositoryId, parentFolderId, _tailgateRootFolder,
+				  _tailgateRootFolder, serviceContext);
+		}
+		return folder;
+	}
+
+	public static Folder getOrCreateFolder(String eventFolderName, long parentFolderId, long repositoryId, long userId, ServiceContext serviceContext) throws PortalException, SystemException{
+		Folder folder= null;
+		if(parentFolderId == 0){
+			  parentFolderId = DLFolderConstants.DEFAULT_PARENT_FOLDER_ID;
+		  }
+		  try {
+			  folder = DLAppLocalServiceUtil.getFolder(
+		            repositoryId, parentFolderId, eventFolderName);
+		  } catch (Exception e) {
+			  folder = DLAppLocalServiceUtil.addFolder(userId,
+		            repositoryId, parentFolderId, eventFolderName,
+		            eventFolderName, serviceContext);
+			  setGuestViewFolderPermission(folder);
+		    }
+		return folder;
+	}
+/**
+ * 	
+ * @param folder
+ * @param sourcefileName
+ * @param title
+ * @param desc
+ * @param file
+ * @param mimeType
+ * @param serviceContext
+ * @return
+ * @throws PortalException
+ * @throws SystemException
+ */
+	public static FileEntry addFileEntry(Folder folder, String sourcefileName,
+										 String title, String desc, File file,  
+										 String mimeType, ServiceContext serviceContext) 
+														 throws PortalException, SystemException{
+		FileEntry fileEntry=null;	
+		try{	
+				fileEntry = DLAppLocalServiceUtil.addFileEntry(serviceContext.getUserId(), folder.getRepositoryId(), 
+													folder.getFolderId(), sourcefileName, mimeType, 
+													title, desc, 
+													"", file, 
+													serviceContext);
+				setGuestViewPermission(fileEntry);
+				
+			}catch(Exception ex){
+			
+			}
+		return fileEntry;
+	}
+
+	public static void setGuestViewPermission( FileEntry fileEntry) throws PortalException, SystemException{
+		ResourcePermission resourcePermission = null;
+		Role guestRole = getGuestRole();
+		try
+		   {
+			
+		    resourcePermission = ResourcePermissionLocalServiceUtil.getResourcePermission(fileEntry.getCompanyId(),
+		    					DLFileEntry.class.getName(),
+		    					ResourceConstants.SCOPE_INDIVIDUAL, 
+		    					String.valueOf(fileEntry.getPrimaryKey()),
+		    					guestRole.getRoleId());
+		        
+		    ResourceAction resourceAction = ResourceActionLocalServiceUtil.getResourceAction(DLFileEntry.class.getName(), ActionKeys.VIEW);
+		  
+		    if(Validator.isNotNull(resourcePermission) && !ResourcePermissionLocalServiceUtil.hasActionId(resourcePermission,resourceAction))
+		    {
+		      resourcePermission.setActionIds(resourcePermission.getActionIds() + resourceAction.getBitwiseValue());
+		      ResourcePermissionLocalServiceUtil.updateResourcePermission(resourcePermission);
+		    }
+		   }catch(Exception ex){
+			      resourcePermission = ResourcePermissionLocalServiceUtil.createResourcePermission(CounterLocalServiceUtil.increment());
+			      resourcePermission.setCompanyId(fileEntry.getCompanyId());
+			      resourcePermission.setName(DLFileEntry.class.getName());
+			      resourcePermission.setScope(ResourceConstants.SCOPE_INDIVIDUAL);
+			      resourcePermission.setPrimKey(String.valueOf(fileEntry.getPrimaryKey()));
+			      resourcePermission.setRoleId(guestRole.getRoleId());
+			    
+			      ResourceAction resourceAction = ResourceActionLocalServiceUtil.getResourceAction(DLFileEntry.class.getName(), ActionKeys.VIEW);
+			      resourcePermission.setActionIds(resourceAction.getBitwiseValue());// (ActionKeys.VIEW);
+			      ResourcePermissionLocalServiceUtil.addResourcePermission(resourcePermission);
+			   
+		   }
+	}
+	public static void setGuestViewFolderPermission( Folder dlFolder) throws PortalException, SystemException{
+		ResourcePermission resourcePermission = null;
+		Role guestRole = getGuestRole();
+		try
+		   {
+		    resourcePermission = ResourcePermissionLocalServiceUtil.getResourcePermission(dlFolder.getCompanyId(),
+		    					DLFolder.class.getName(),
+		    					ResourceConstants.SCOPE_INDIVIDUAL, 
+		    					String.valueOf(dlFolder.getPrimaryKey()),
+		    					guestRole.getRoleId());
+		        
+		    ResourceAction resourceAction = ResourceActionLocalServiceUtil.getResourceAction(DLFolder.class.getName(), ActionKeys.VIEW);
+		  
+		    if(Validator.isNotNull(resourcePermission) && !ResourcePermissionLocalServiceUtil.hasActionId(resourcePermission,resourceAction))
+		    {
+		      resourcePermission.setActionIds(resourcePermission.getActionIds() + resourceAction.getBitwiseValue());
+		      ResourcePermissionLocalServiceUtil.updateResourcePermission(resourcePermission);
+		    }
+		   }catch(Exception ex){
+			      resourcePermission = ResourcePermissionLocalServiceUtil.createResourcePermission(CounterLocalServiceUtil.increment());
+			      resourcePermission.setCompanyId(dlFolder.getCompanyId());
+			      resourcePermission.setName(DLFolder.class.getName());
+			      resourcePermission.setScope(ResourceConstants.SCOPE_INDIVIDUAL);
+			      resourcePermission.setPrimKey(String.valueOf(dlFolder.getPrimaryKey()));
+			      resourcePermission.setRoleId(guestRole.getRoleId());
+			    
+			      ResourceAction resourceAction = ResourceActionLocalServiceUtil.getResourceAction(DLFolder.class.getName(), ActionKeys.VIEW);
+			      resourcePermission.setActionIds(resourceAction.getBitwiseValue());// (ActionKeys.VIEW);
+			      ResourcePermissionLocalServiceUtil.addResourcePermission(resourcePermission);
+		   }
+	}
+	/**
+	 * 
+	 * @param fullFileName
+	 * @return
+	 */
+	
+	public static String getMimeType(String fullFileName){
+		Path source = Paths.get(fullFileName);
+		String mimeType="";
+		try {
+			mimeType = Files.probeContentType(source);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return mimeType;
+	}
+}
---
+++ liferay-plugins-sdk-6.2/portlets/flask-user-tailgate-portlet/docroot/WEB-INF/src/com/rumbasolutions/flask/managetailgate/ManageTailgatePortletAction.java
@@ -0,0 +1,138 @@
+package com.rumbasolutions.flask.managetailgate;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.portlet.PortletFileUpload;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+//import com.rumbasolutions.flask.service.TailgateInfoLocalServiceUtil;
+
+public class ManageTailgatePortletAction extends MVCPortlet {
+	
+	
+	/**
+	 * @param actionRequest
+	 * @param actionResponse
+	 * @throws IOException
+	 * @throws PortletException
+	 */
+	private static final String UPLOAD_DIRECTORY = "upload";
+	private static final int THRESHOLD_SIZE = 1024 * 1024 * 10; // 3MB
+	private static final int MAX_FILE_SIZE = 1024 * 1024 * 10; // 10MB
+	private static final int REQUEST_SIZE = 1024 * 1024 * 50; // 50MB
+	
+	private static Log LOGGER = LogFactoryUtil.getLog(ManageTailgatePortletAction.class);
+	
+	private final String TAILGATE_ID_QSTR = "_tailgateId";
+	private long _tailgateId = 0;
+	
+	private ServiceContext _serviceContext;
+	Folder _eventFolder=null;
+	
+	public void addImages(ActionRequest actionRequest, ActionResponse actionResponse)throws IOException, PortletException {
+		
+		if (!PortletFileUpload.isMultipartContent( actionRequest)) {
+			return;
+		}
+	
+		_serviceContext = getServicContext(actionRequest);
+		// configures some settings
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(THRESHOLD_SIZE);
+		factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+		
+		PortletFileUpload upload = new PortletFileUpload(factory);
+		upload.setFileSizeMax(MAX_FILE_SIZE);
+		upload.setSizeMax(REQUEST_SIZE);
+		
+		// constructs the directory path to store upload file
+		String uploadPath = getPortletContext().getRealPath("")
+			+ File.separator + UPLOAD_DIRECTORY;
+		
+		
+		try {
+			// parses the request's content to extract file data
+			List<FileItem> formItems = upload.parseRequest(actionRequest);
+			_tailgateId = getEventId(formItems);
+			createUploadFolder(uploadPath);
+			
+			for(FileItem item: formItems){
+				if (!item.isFormField()) {
+					String fileName = _tailgateId +"_" + new File(item.getName()).getName();
+					String fileTitle = fileName;
+					String fileDesc = fileName; // Change is later for description 
+					String filePath = uploadPath + File.separator + fileName;
+					File storeFile = new File(filePath);
+					// saves the file on disk
+					item.write(storeFile);
+					String mimeType = FlaskDocLibUtil.getMimeType(filePath);
+					FileEntry fileEntry = FlaskDocLibUtil.addFileEntry(_eventFolder, fileName, fileTitle, fileDesc, storeFile, mimeType, _serviceContext);
+					//TailgateInfoLocalServiceUtil.addEventDetailImage(_eventDetailId, fileTitle, fileDesc, fileEntry.getUuid(), fileEntry.getGroupId(), _serviceContext);
+					
+				}else{
+					
+				}
+			}
+			actionRequest.setAttribute("message", "Upload has been done successfully!");
+		} catch (Exception ex) {
+			actionRequest.setAttribute("message", "There was an error: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+	private void createUploadFolder(String uploadPath)  {
+		// creates the directory if it does not exist
+		File uploadDir = new File(uploadPath);
+		if (!uploadDir.exists()) {
+			uploadDir.mkdir();
+		}
+		try {
+			_eventFolder = FlaskDocLibUtil.createTailgateFolder(_tailgateId,_serviceContext);
+		}
+		catch (Exception e) {
+			LOGGER.error("Error in creating tailgate folder in Document Media Library", e);
+		}
+	}
+
+	private ServiceContext getServicContext(ActionRequest actionRequest){
+		
+		ServiceContext serviceContext=null;
+		try {
+			serviceContext = ServiceContextFactory.getInstance(actionRequest);
+		}
+		catch (PortalException e) {
+			LOGGER.error(e);	
+		}
+		catch (SystemException e) {
+			LOGGER.error(e);
+		}
+		return serviceContext;
+	}
+	
+	private long getEventId(List<FileItem> formItems){
+		long eventId = 0;
+		for (FileItem item : formItems){
+			if(item.getFieldName().contentEquals(TAILGATE_ID_QSTR)){
+				eventId = Long.parseLong(item.getString());
+				break;
+			}
+		}
+		return eventId;
+	}
+}
