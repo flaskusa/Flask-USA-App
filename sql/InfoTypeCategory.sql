5ce12d6350402aab2794a501abc35d581f24f50c
arai <arai@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-11 17:40:56 +0000

    removed flask. from the table name
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@962 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- sql/InfoTypeCategory.sql
+++ sql/InfoTypeCategory.sql
@@ -1,47 +1,47 @@
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (1, "Parking","", 1 );
 											
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (2, "Traffic","", 1 );		
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (3, "Safety","", 1 );		
 											
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (4, "Bar & Restaurants","", 1 );		
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (5, "Liquor Store","", 1 );			
 											
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (6, "Tradition","", 1 );			
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (7, "Supplies","", 1 );														
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (8, "Flask Us","", 1 );														
 
 											
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (101, "Venue Map","", 2 );														
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (102, "Bar & Restaurants","", 2 );			
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (103, "Flask Us","", 2 );	
 											
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (201, "Nightlife","", 3 );													
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (202, "Bar & Restaurants","", 3 );			
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (203, "Getting home","", 3 );		
 
-INSERT INTO flask.flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
+INSERT INTO flaskevents_infotypecategory (infoTypeCategoryId,infoTypeCategoryName, displayTemplate, infoTypeId)
 											VALUES (204, "Flask Us","", 3 );												
 
 
