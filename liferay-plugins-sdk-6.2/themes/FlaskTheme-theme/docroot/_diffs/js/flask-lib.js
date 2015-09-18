15562a95d10bcbd3135c4c89c71f1b781e3a33a0
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-18 16:17:06 +0000

    Services added for view ad - Giri
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1004 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/themes/FlaskTheme-theme/docroot/_diffs/js/flask-lib.js
+++ liferay-plugins-sdk-6.2/themes/FlaskTheme-theme/docroot/_diffs/js/flask-lib.js
@@ -8,14 +8,18 @@ _flaskLib.UTILITY = {
 }
 
 _flaskLib.SERVICE_ENDPOINTS = {
-				API_PREFIX					: "/api/jsonws",
-				GET_COUNTRIES 				: "/flask-rest-users-portlet.flaskadmin/get-countries",
-				GET_REGION 					: "/flask-rest-users-portlet.flaskadmin/get-region",
-				GET_USA_REGION 				: "/flask-rest-users-portlet.flaskadmin/get-usa-regions",
-				DELETE_FILES				: "/dlapp/delete-file-entry",
-				GET_FILE_ENTRY_ID   		: "/dlapp/get-file-entry-by-uuid-and-group-id",
-				GET_EVENT_TYPES 			: "/flask-rest-events-portlet.eventtype/get-event-types"
-
+		API_PREFIX					: "/api/jsonws",
+		GET_COUNTRIES 				: "/flask-rest-users-portlet.flaskadmin/get-countries",
+		GET_REGION 					: "/flask-rest-users-portlet.flaskadmin/get-region",
+		GET_USA_REGION 				: "/flask-rest-users-portlet.flaskadmin/get-usa-regions",
+		DELETE_FILES				: "/dlapp/delete-file-entry",
+		GET_FILE_ENTRY_ID   		: "/dlapp/get-file-entry-by-uuid-and-group-id",
+		GET_EVENT_TYPES 			: "/flask-rest-events-portlet.eventtype/get-event-types",
+		GET_EVENT_CAMPAIGNS			: "/flask-advertisement-portlet.campaignevent/get-event-campaigns",
+		GET_CAMPAIGN_EVENTS			: "/flask-advertisement-portlet.campaignevent/get-campaign-events",
+		GET_CAMPAIGN_EVENTS			: "/flask-advertisement-portlet.campaignevent/get-campaign-events",
+		GET_CAMPAIGN_IMAGES			: "/flask-advertisement-portlet.adcampaign/get-campaign-with-images"
+			
 }
 
 _flaskLib.getFormData = function(formId, model, customGetData){
