1d5563920b84689ee087633ecacbbf0f2b2cfe4d
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-18 16:15:34 +0000

    New file for showing ads - Giri
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1003 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-advertisement.js
@@ -0,0 +1,56 @@
+var _flaskAd = {};
+
+_flaskAd.UTILITY = {
+		AUTOPLAY		: 3000,
+		GROUP_ID		:Liferay.ThemeDisplay.getSiteGroupId()
+}
+
+_flaskAd.ShowAdByEventId = function(eventId){
+	var flaskRequest = new Request();
+	var param = {eventId: eventId};
+	flaskRequest.sendGETRequest(_flaskLib.SERVICE_ENDPOINTS.GET_EVENT_CAMPAIGNS , param, 
+		function (data){
+			var campaignIds = [];
+			for(var iCount=0;iCount<data.length;iCount++){
+				campaignIds.push(data[iCount].campaignId);
+			}
+			_flaskAd.GetAdImages(campaignIds)			
+		} ,
+		function (data){
+			console.log("Error in getting ads: " + data );
+	});
+}
+
+_flaskAd.GetAdImages = function(Obj){
+	var adContainer = $("<div/>");
+	adContainer.attr('id','fixedfooter');
+	adContainer.owlCarousel({
+		items:3,
+		//navigation:true,
+		navigationText:["<i class='icon-chevron-left icon-white'></i>","<i class='icon-chevron-right icon-white'></i>"],
+		pagination:true,
+		autoPlay : true,
+		slideSpeed : 200
+	});
+
+	var imageUUIDArray = [];
+	for(var iCount=0;iCount<Obj.length;iCount++){
+		var campaignId = Obj[iCount];
+		var flaskRequest = new Request();
+		var param = {campaignId: campaignId};
+		flaskRequest.sendGETRequest(_flaskLib.SERVICE_ENDPOINTS.GET_CAMPAIGN_IMAGES , param, 
+			function (data){
+				var objCampaignImages = data.campaignImages
+				for(var iCount=0;iCount<objCampaignImages.length;iCount++){
+					var objCampaignImage = $.parseJSON(objCampaignImages[iCount].campaignImage);
+					var imageURL = _flaskLib.UTILITY.IMAGES_PATH + "?uuid=" + objCampaignImage.imageUUID + "&groupId="+_flaskAd.UTILITY.GROUP_ID;
+					var adSlide = $('<div/>',{'class':'adSlide','style':'background-image:url('+imageURL+');background-size: inherit;min-height: 95px;'});					
+					adContainer.data('owlCarousel').addItem(adSlide);
+					$("body").append(adContainer);
+				}
+			} ,
+			function (data){
+				console.log("Error in getting ads: " + data );
+		});		
+	}
+}
