cf794919f817ec80cb5f9548b3c169166b2fc183
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-19 10:36:28 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1011 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-advertisement.js
+++ liferay-plugins-sdk-6.2/portlets/flask-view-events-portlet/docroot/js/flask-advertisement.js
@@ -25,7 +25,6 @@ _flaskAd.GetAdImages = function(Obj){
 	adContainer.attr('id','fixedfooter');
 	adContainer.owlCarousel({
 		items:3,
-		//navigation:true,
 		navigationText:["<i class='icon-chevron-left icon-white'></i>","<i class='icon-chevron-right icon-white'></i>"],
 		pagination:true,
 		autoPlay : true,
@@ -39,12 +38,26 @@ _flaskAd.GetAdImages = function(Obj){
 		var param = {campaignId: campaignId};
 		flaskRequest.sendGETRequest(_flaskLib.SERVICE_ENDPOINTS.GET_CAMPAIGN_IMAGES , param, 
 			function (data){
-				var objCampaignImages = data.campaignImages
+				var objCampaignDetails = $.parseJSON(data.AdCampaign);
+				var objCampaignImages = data.campaignImages;
 				for(var iCount=0;iCount<objCampaignImages.length;iCount++){
 					var objCampaignImage = $.parseJSON(objCampaignImages[iCount].campaignImage);
 					var imageURL = _flaskLib.UTILITY.IMAGES_PATH + "?uuid=" + objCampaignImage.imageUUID + "&groupId="+objCampaignImage.imageGroupId;
-					var adSlide = $('<div/>',{'class':'adSlide','style':'background-image:url('+imageURL+');background-size: inherit;min-height: 95px;'});					
+					var adSlide = $('<div/>',{'class':'adSlide','data-fullImageURL':imageURL,'data-title':objCampaignDetails.campaignName});
+					var adSlideImage = $('<img/>',{'src':imageURL});
+					adSlide.append(adSlideImage);
 					adContainer.data('owlCarousel').addItem(adSlide);
+					adSlide.click(function(){
+						var fullImageURL = $(this).attr('data-fullImageURL');
+						var campaignTitle = $(this).attr('data-title');
+						var objImage = $('<img/>',{'src':fullImageURL});
+						var imgContainer = $('.imageContainer');
+						$('#myModalLabel').html("");
+						$('#myModalLabel').html(campaignTitle);						
+						imgContainer.html("");
+						imgContainer.append(objImage);
+						$('#myModal').modal('show');
+					});
 					$("body").append(adContainer);
 				}
 			} ,
