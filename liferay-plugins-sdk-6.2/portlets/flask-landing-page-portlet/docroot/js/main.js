46ed73aa6333cdd9bf9313132ca8a82f0344c659
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-29 13:51:00 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1147 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-landing-page-portlet/docroot/js/main.js
+++ liferay-plugins-sdk-6.2/portlets/flask-landing-page-portlet/docroot/js/main.js
@@ -38,4 +38,28 @@ $(document).ready(function(){
 		$(".row3").hide();
 		$(".row4").show();
 	});
+	$("#whatIsInFlask").click(function(){
+		$(".row1").hide();
+		$(".row2").hide();
+		$(".row3").show();
+		$(".row4").hide();
+	});
+	
+	$("#Ok").click(function(){
+		contact($("#name").val(),$("#email").val(),$("#mobile").val(),$("#comment").val());
+	});
+	
 });
+
+function contact(name, email, mobile, comment) {
+	var param = {'name': name, 'email': email, 'mobile': mobile, 'comment': comment};
+	var request = new Request();
+	var flaskRequest = new Request();
+	flaskRequest.sendPOSTRequest('/flask-email-notification-portlet.invitationinfo/reply-to-feedback' , param,
+		function(data) {
+				console.log("Email sent successfully");
+		} ,
+		function(data) {
+				console.log(data);
+		});
+}
