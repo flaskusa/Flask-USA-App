10e6e3bbaa3b36656f62f98792dc4b82c7ef3886
gpawar <gpawar@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2015-09-29 13:52:22 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1149 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- liferay-plugins-sdk-6.2/portlets/flask-email-notification-portlet/docroot/WEB-INF/sql/tables.sql
+++ liferay-plugins-sdk-6.2/portlets/flask-email-notification-portlet/docroot/WEB-INF/sql/tables.sql
@@ -1,7 +1,7 @@
 create table flaskinvitation_InvitationInfo (
 	inviationId LONG not null primary key,
-	name VARCHAR(75) null,
-	email VARCHAR(75) null,
-	description VARCHAR(75) null,
+	name VARCHAR(100) null,
+	email VARCHAR(255) null,
+	description VARCHAR(2000) null,
 	invitedDate VARCHAR(75) null
 );
