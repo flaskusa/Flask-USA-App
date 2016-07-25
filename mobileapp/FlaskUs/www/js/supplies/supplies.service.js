12c2b0c5a0d181c112bd9158f600814684d8d463
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-07-25 16:09:25 +0000

    added supply item list
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1575 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/supplies/supplies.service.js
+++ mobileapp/FlaskUs/www/js/supplies/supplies.service.js
@@ -36,19 +36,28 @@
     this.data2 = [
         {
             listid: 1,
-            itemName: "TICKETS"
+            itemName: "TICKETS",
+            checked : false
         },
         {
             listid: 2,
-            itemName: "BLANKETS"
+            itemName: "BLANKETS",
+            checked: true
         },
         {
             listid: 3,
-            itemName: "PARKING PASS"
+            itemName: "PARKING PASS",
+            checked: false
         },
         {
             listid: 4,
-            itemName: "CASH/WALLET"
+            itemName: "CASH/WALLET",
+            checked: true
+        },
+        {
+            listid: 5,
+            itemName: "UMBRELLA",
+            checked: true
         }
     ];
 
@@ -79,6 +88,34 @@
         // increment by one
         return maxId + 1;
     };
+    // for supplies list items
+    this.getData2 = function () {
+        return this.data2;
+    };
+
+    this.setData2 = function (data2) {
+        this.data2 = data2;
+    };
+
+    // add a new data item that does not exist already
+    // must compute a new unique id and backfill in
+    this.addOne1 = function (dataItem1) {
+        // must calculate a unique ID to add the new data
+        var newId1 = this.newId();
+        dataItem1.listid1 = newId1;
+        this.data2.push(dataItem1);
+        return dataItem1;
+    };
+
+    // return an id to insert a new data item at
+    this.newId1 = function () {
+        // find all current ids
+        var arr1 = this.data2;
+        var maxId1 = arr1.length;
+        console.log(maxId1);
+        // increment by one
+        return maxId1 + 1;
+    };
 
 })
 
@@ -91,12 +128,25 @@ angular.module('flaskApp').factory('HttpService', function ($http) {
         get: function (id) {
             return $http.get('/suppliesList/' + id);
         },
-        // making save dual-function like default ngResource behavior (no separate update w/PUT)
+        get2: function (id) {
+            return $http.get('/suppliesList1/' + id);
+        },
+        // making save data1
         save: function (data) {
-            if (angular.isDefined(data.listid)) {
+            console.log(data);
+            if (angular.isDefined(data.listid)) {               
                 return $http.post('/suppliesList/' + data.listid, data);
             } else {
                 return $http.post('/suppliesList', data);
+            }
+        },
+        // making save for data2
+        save2: function (data2) {
+            console.log(data2);
+            if (angular.isDefined(data2.listid)) {
+                return $http.post('/suppliesList1/' + data2.listid, data2);
+            } else {
+                return $http.post('/suppliesList1', data2);
             }
         },
         delete: function (id) {
