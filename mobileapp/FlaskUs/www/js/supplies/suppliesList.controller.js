b6f0516394328b4f76b61bdddf35baf0fdaea0ff
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-07-25 12:42:12 +0000

    Supplies item list controller
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1563 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ mobileapp/FlaskUs/www/js/supplies/suppliesList.controller.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+    angular.module('flaskApp')
+
+    .controller('SuppliesListCtrl', SuppliesListCtrl);
+
+    SuppliesListCtrl.$inject = ['$scope', '$stateParams', 'HttpService', 'ServerDataModel', '$ionicModal', '$ionicNavBarDelegate'];
+
+    function SuppliesListCtrl($scope, $stateParams, HttpService, ServerDataModel, $ionicModal, $ionicNavBarDelegate) {
+        /* jshint validthis: true */
+        // putting our server data on scope to display
+        $scope.dataModel = ServerDataModel;
+        
+        var currListName = $stateParams.listName
+
+        $ionicNavBarDelegate.title(currListName);
+
+        $ionicModal.fromTemplateUrl('templates/modal.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.saveList2 = function (list) {
+            console.log(list);
+            HttpService.save2(list);
+            $scope.modal.hide();
+        };
+    }
+})();
