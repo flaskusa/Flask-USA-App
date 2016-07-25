d18c80917c6bca823684ca2a274ab76b5c44bad8
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-07-25 16:11:57 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1576 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/supplies/supplies.backend.js
+++ mobileapp/FlaskUs/www/js/supplies/supplies.backend.js
@@ -15,17 +15,27 @@
             var lists = ServerDataModel.findAll();
             return [200, lists, {}];
         });
+        $httpBackend.whenGET('/suppliesList1').respond(function (method, url, data2) {
+            var lists = ServerDataModel.findAll();
+            return [200, lists, {}];
+        });
 
         // this is the creation of a new resource
         $httpBackend.whenPOST('/suppliesList').respond(function (method, url, data) {
             var params = angular.fromJson(data);
-
             var list = ServerDataModel.addOne(params);
-
             // get the id of the new resource to populate the Location field
             var listid = list.listid;
-
             return [201, list, { Location: '/suppliesList/' + listid }];
+        });
+
+        // this is the creation of a new resource
+        $httpBackend.whenPOST('/suppliesList1').respond(function (method, url, data2) {
+            var params = angular.fromJson(data2);
+            var list1 = ServerDataModel.addOne1(params);
+            // get the id of the new resource to populate the Location field
+            var listid1 = list1.listid1;
+            return [201, list1, { Location: '/suppliesList1/' + listid1 }];
         });
 
         $httpBackend.whenGET(/templates\//).passThrough();
