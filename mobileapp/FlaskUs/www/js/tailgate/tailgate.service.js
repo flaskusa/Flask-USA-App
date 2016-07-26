1885d2ec6d2a999f618838460571e18998665f42
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-07-26 16:04:36 +0000

    Added tailgate files
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1581 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ mobileapp/FlaskUs/www/js/tailgate/tailgate.service.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('flaskApp')
+        .service('TailgateService', TailgateService);
+
+    TailgateService.$inject = ['$http'];
+
+    function TailgateService($http) {
+        var baseURL = "http://test@liferay.com:test@146.148.83.30/api/jsonws";
+        var allTailgateURL = "/flask-user-tailgate-portlet.tailgateinfo/get-all-tailgate";
+        var myTailgatesURL = "/flask-user-tailgate-portlet.tailgateinfo/get-all-my-tailgate";
+
+        this.getAllTailgate = getAllTailgate
+
+        function getAllTailgate() {
+            return $http.get(baseURL + allTailgateURL)
+                .then(function success(response) {
+                    return response;
+                }, function failure(response) {
+                    //add errror handling 
+                });
+        }
+
+        this.getMyTailgates = getMyTailgates
+
+        function getMyTailgates() {
+            return $http.get(baseURL + myTailgatesURL, {
+                params: { 'userId': '20198' }
+                }
+            )
+            .then(function success(response) {
+                console.log(response);
+                return response;
+            }, function failure(response) {
+                //add errror handling 
+            });
+        }
+    }
+})();
