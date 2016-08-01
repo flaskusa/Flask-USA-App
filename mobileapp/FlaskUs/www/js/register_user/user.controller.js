35bece9aeed9d47e9233254a7d4349a38e2cbda5
savitha <savitha@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-01 12:24:02 +0000

    controller and services for user_registration
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1625 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ mobileapp/FlaskUs/www/js/register_user/user.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    angular.module('flaskApp')
+        .controller('user_registrationCtrl', user_registrationCtrl);
+
+    user_registrationCtrl.$inject = ['$scope', 'UserService'];
+
+    /* @ngInject */
+    function user_registrationCtrl($scope, UserService) {
+        $scope.saveUser = function (user) {
+            console.log(user);
+            if (user.gender_male == "male") {
+                var male = true;
+                $scope.isMale = male;
+            }
+            console.log(male);
+          UserService.saveUser(user);
+        }
+    }
+})();
+
---
+++ mobileapp/FlaskUs/www/js/register_user/user.service.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('flaskApp')
+        .service('UserService', UserService);
+
+    UserService.$inject = ['$http'];
+
+    function UserService($http) {
+        var baseURL = "http://192.168.1.136:8080/api/jsonws";
+        var addUserURL = "/flask-rest-users-portlet.flaskadmin/sign-up";
+
+        this.saveUser = saveUser
+
+        function saveUser(user) {
+            
+            console.log(user);
+            var params= {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                screenName: user.screenName,
+                password1: user.password1,
+                password2: user.password2,
+                DOB: '25-1-1991',
+                isMale: true,
+                areaCode: user.areaCode,
+                mobileNumber: '67676676767'
+            };
+            return $http.get(baseURL + addUserURL, {
+                params
+            }
+            )
+            .then(function success(response) {
+                    console.log(response);
+                    return response;
+                }, function failure(response) {
+                    //add errror handling 
+                });
+        }
+    }
+})();
+
+
