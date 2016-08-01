7f472d12e6ea1088052412dc5d48f4e36694df9a
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-01 16:23:21 +0000

    added new prepost game page controller
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1637 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ mobileapp/FlaskUs/www/js/events/prePostGame.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+    angular.module('flaskApp')
+            .controller('prePostGameCtrl', prePostGameCtrl);
+
+    prePostGameCtrl.$inject = ['$scope', '$stateParams', 'EventsService', '$ionicSlideBoxDelegate'];
+
+    /* @ngInject */
+    function prePostGameCtrl($scope, $stateParams, EventsService, $ionicSlideBoxDelegate) {
+        /* jshint validthis: true */
+        var self = this;
+        $scope.eventInfo = [];
+        console.log($stateParams);
+        $scope.currEventName = $stateParams.eventName;
+        $scope.currEventId = $stateParams.eventId;
+        var currEventId = $scope.currEventId;
+        console.log(currEventId);
+        getAllVenuImages();
+
+        function getAllVenuImages() {
+            EventsService.getVenueImages(currEventId).then(function (respData) {
+                $scope.currEventData = respData.Event;
+            });
+        }
+    }
+})();
