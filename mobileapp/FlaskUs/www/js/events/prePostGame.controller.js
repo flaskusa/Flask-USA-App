9857d4a3dffac2bc886c47b7355cb03bd3545728
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-02 15:17:30 +0000

    change for prepost game
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1652 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/events/prePostGame.controller.js
+++ mobileapp/FlaskUs/www/js/events/prePostGame.controller.js
@@ -10,6 +10,12 @@
         /* jshint validthis: true */
         var self = this;
         $scope.eventInfo = [];
+        $scope.Details = [];
+        $scope.INFO_TYPE = [];
+        $scope.INFO_TYPE_CATEGORY = [];
+        $scope.Pre_Game = [];
+        $scope.Post_Game = [];
+        $scope.During_Game = [];
         console.log($stateParams);
         $scope.currEventName = $stateParams.eventName;
         $scope.currEventId = $stateParams.eventId;
@@ -19,8 +25,33 @@
 
         function getAllVenuImages() {
             EventsService.getVenueImages(currEventId).then(function (respData) {
-                $scope.currEventData = respData.Event;
+                $scope.currEventData = respData.data.Event;
+                $scope.currVenueData = respData.data.Venue;
+                var len = respData.data.Details.length - 1;
+                for (var i = 0; i <= len; i++) {                    
+                    $scope.Details.push(angular.fromJson(respData.data.Details[i].Detail))
+                    if ($scope.INFO_TYPE.indexOf($scope.Details[i].infoTypeName) == -1) {
+                        $scope.INFO_TYPE.push($scope.Details[i].infoTypeName)
+                    }
+                }
+                for (var i = 0; i <= len; i++) {
+                    if ($scope.Details[i].infoTypeName == "Pre-Event") {
+                        if ($scope.Pre_Game.indexOf($scope.Details[i].infoTypeCategoryName) == -1) {
+                            $scope.Pre_Game.push($scope.Details[i].infoTypeCategoryName)
+                        }
+                    }else
+                        if ($scope.Details[i].infoTypeName == "Post-Event") {
+                            if ($scope.Post_Game.indexOf($scope.Details[i].infoTypeCategoryName) == -1) {
+                                $scope.Post_Game.push($scope.Details[i].infoTypeCategoryName)
+                        }
+                    } else {
+                            if ($scope.During_Game.indexOf($scope.Details[i].infoTypeCategoryName) == -1) {
+                                $scope.During_Game.push($scope.Details[i].infoTypeCategoryName)
+                        }
+                    }
+                    
+                }
             });
         }
     }
-})();
+})(); 
