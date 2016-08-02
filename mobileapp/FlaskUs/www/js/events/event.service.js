ad708c55bb643f7321d9817bf8cba3e9c7fc9224
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-02 15:18:09 +0000

    change in service
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1653 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/events/event.service.js
+++ mobileapp/FlaskUs/www/js/events/event.service.js
@@ -9,16 +9,25 @@
 
     function EventsService($http, REST_API) {
         var baseURL = REST_API.url;
-        var allEventsURL = "flask-rest-events-portlet.event/get-all-events";
-        var addEventURL = "/flask-rest-events-portlet.event/add-event";
-        var addEventdetailsURL = "/flask-rest-events-portlet.event/add-event-detail";
-        var getFilteredEvents = "/flask-rest-events-portlet.event/get-simple-filtered-events";
-        var getEvent_Venue_Details_with_Images = "/flask-rest-events-portlet.event/get-event-venue-details-with-images";
+        var allEventsURL = "flask-rest-events-portlet.event/get-simple-filtered-events";
+        var addEventURL = "flask-rest-events-portlet.event/add-event";
+        var addEventdetailsURL = "flask-rest-events-portlet.event/add-event-detail";
+        var getFilteredEvents = "flask-rest-events-portlet.event/get-simple-filtered-events";
+        var getEvent_Venue_Details_with_Images = "flask-rest-events-portlet.event/get-event-venue-details-with-images";
 
         this.getAllEvents = getAllEvents;
         this.getVenueImages = getVenueImages;
         function getAllEvents() {
-            return $http.get(baseURL + allEventsURL)
+            return $http.get(baseURL + allEventsURL, {
+                params: {
+                    eventTypeIds: '',
+                    startDate: '',
+                    endDate: '',
+                    searchString: 'b',
+                    latitude: '',
+                    longitude: ''
+                }
+            })
                 .then(function success(response) {
                     return response;
                 }, function failure(response) {
