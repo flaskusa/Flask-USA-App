755e55d61b6b45f8a1cc2de9502dba36c86d93db
savitha <savitha@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-08 15:33:16 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1708 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/login/login.controller.js
+++ mobileapp/FlaskUs/www/js/login/login.controller.js
@@ -3,33 +3,48 @@
     angular.module('flaskApp')
         .controller('LoginCtrl', LoginCtrl);
 
-    LoginCtrl.$inject = ['$scope', 'LoginService', '$state', '$ionicPopup', '$timeout'];
-
+    LoginCtrl.$inject = ['$scope', 'LoginService', '$state', '$ionicPopup', '$timeout', '$rootScope'];
+    
 
     /* @ngInject */
-    function LoginCtrl($scope, LoginService, $state, $ionicPopup, $timeout) {
+    function LoginCtrl($scope, LoginService, $state, $ionicPopup, $timeout, $rootScope) {
+        
         /* jshint validthis: true */
         var self = this;
-
         $scope.doLogin = function ($scope) {
             LoginService.authenticateUser($scope).then(function (respData) {
-                console.log(respData.data.message);
+                console.log(respData);
                 // $scope.user = respData.data;
                 if (respData.data.message == "Authenticated access required") {
                     console.log("Authentication failed");
-                    $state.go("app.login");
+                    
                     var myPopup = $ionicPopup.show({
-                        title: '<p class="login_error">Incorrect Username or Password</p>'
+                        title: '<p class="login_error">Incorrect Username or Password</p>',  
                     });
 
                     $timeout(function () {
-                        myPopup.close(); //close the popup after 3 seconds for some reason
+                        myPopup.close();
+                        
                     }, 3000);
                     document.login_form.reset();
                 }
 
+                else if (respData.data.emailAddress == "") {
+                    var myPopup = $ionicPopup.show({
+                        title: '<p class="login_error">Username or Password is empty</p>',
+                    });
+
+                    $timeout(function () {
+                        myPopup.close();
+                        
+                    }, 3000);
+                   
+                }
+
                 else {
                     $state.go("app.events");
+                    $scope.Email = "";
+                    $scope.password = "";
                     document.login_form.reset();
                 }
             });
