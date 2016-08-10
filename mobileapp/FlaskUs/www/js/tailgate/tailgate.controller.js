ae8e22a38b1c8f8307e57012a0f9f542455a0c8e
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-10 10:59:54 +0000

    userid from cookies
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1731 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/tailgate/tailgate.controller.js
+++ mobileapp/FlaskUs/www/js/tailgate/tailgate.controller.js
@@ -3,18 +3,20 @@
     angular.module('flaskApp')
         .controller('my_tailgateCtrl', my_tailgateCtrl);
 
-    my_tailgateCtrl.$inject = ['$scope', 'TailgateService', '$state', '$ionicSlideBoxDelegate'];
+    my_tailgateCtrl.$inject = ['$scope', 'TailgateService', '$state', '$ionicSlideBoxDelegate', '$cookieStore'];
 
     /* @ngInject */
-    function my_tailgateCtrl($scope, TailgateService, $state, $ionicSlideBoxDelegate) {
+    function my_tailgateCtrl($scope, TailgateService, $state, $ionicSlideBoxDelegate, $cookieStore) {
         var self = this;
         $scope.myTailgate = [];
-
+        var userResponse = $cookieStore.get('CurrentUser');
+        var UserId = userResponse.data.userId;
         getAlltailgates();
 
         function getAlltailgates() {
-            TailgateService.getMyTailgates().then(function (respData) {
+            TailgateService.getMyTailgates(UserId).then(function (respData) {
                 $scope.allTailgate = respData.data;
+                console.log(respData.data);
             });
         }
     }
