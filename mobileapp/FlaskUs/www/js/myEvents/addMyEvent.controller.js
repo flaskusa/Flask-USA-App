234ccf7104f82adf6150acc2916dd7ec62e3ac8f
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-18 15:40:23 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1785 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/myEvents/addMyEvent.controller.js
+++ mobileapp/FlaskUs/www/js/myEvents/addMyEvent.controller.js
@@ -8,12 +8,18 @@
     /* @ngInject */
     function addMyEventCtrl($scope, $state, myEventService, $timeout) {
         $scope.myEvent = [];
-        console.log("Inside service");
+        $scope.myFilteredEvent = [];
         addEvent();
         function addEvent() {
             myEventService.getAllEvents().then(function (respData) {
                 $scope.myEvent = respData.data.Events;
                 console.log(respData.data.Events);
+
+                for (var i = 0; i<=$scope.myEvent.length; i++){
+                    if ($scope.myEvent[i].userEvent == "0") {
+                        $scope.myFilteredEvent.push($scope.myEvent[i]);
+                    }
+                }
             });
         }
         
@@ -22,8 +28,7 @@
         $scope.addmyEvent = function (eventId, index) {
             myEventService.addUserEvent(eventId).then(function (respData) {
                 //$scope.myEvent = respData.data.Events;
-                $scope.myEvent.splice(index, 1);
-                console.log(respData);
+                $scope.myFilteredEvent.splice(index, 1);
                 $scope.AddedSuccess = true;
                 $timeout(function () { $scope.AddedSuccess = false; }, 3000);
             });
