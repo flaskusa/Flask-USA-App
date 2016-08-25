7b63492797415201f56206be0b4b26b8ec004b46
rajesh <rajesh@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-25 06:08:49 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1856 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/friends/friendDetails.controller.js
+++ mobileapp/FlaskUs/www/js/friends/friendDetails.controller.js
@@ -3,13 +3,13 @@
     angular.module('flaskApp')
         .controller('FriendDetailCtrl', FriendDetailCtrl);
 
-    FriendDetailCtrl.$inject = ['$scope','$stateParams','FriendsService','$flaskUtil','SERVER'];
+    FriendDetailCtrl.$inject = ['$scope','$stateParams','FriendsService','$flaskUtil','SERVER','$state'];
 
     /* @ngInject */
-    function FriendDetailCtrl($scope,$stateParams,FriendsService, $flaskUtil,SERVER) {
+    function FriendDetailCtrl($scope,$stateParams,FriendsService, $flaskUtil,SERVER,$state) {
         $scope.userId = $stateParams.userId;
         $scope.friend = {};
-        $scope.picUrl = SERVER.hostName+"c/document_library/get_file?uuid=";
+        $scope.picUrl = SERVER.url+"c/document_library/get_file?uuid=";
         $scope.initialize = function() {
             $scope.getFriendByUserId($scope.userId);
         }; 
@@ -22,7 +22,22 @@
                     $flaskUtil.alert("Failed to load details");
                 }
             })
-            }
+        };
+
+        $scope.blockFriend = function() {
+            FriendsService.blockUser($scope.friend.userId).then(function(res) {
+                $state.go("app.my_friends");
+            });
+        };
+         $scope.sendMessage = function() {
+             FriendsService.sendMessage().then(function(res) {
+            });
+        };
+         $scope.unFriend = function() {
+            FriendsService.unFriend($scope.friend.userId).then(function(res) {
+                $state.go("app.my_friends");
+            });
+        };
             $scope.initialize();
         }
 
--- mobileapp/FlaskUs/www/js/friends/friends.controller.js
+++ mobileapp/FlaskUs/www/js/friends/friends.controller.js
@@ -8,23 +8,81 @@
     /* @ngInject */
     function FriendsCtrl($scope, $http, $ionicModal,FriendsService,$flaskUtil) {
       $scope.myFriends = [];
+      $scope.userContactList = [];
       $scope.startIndex = 0;
       $scope.endIndex = 9;
       $scope.noFriendAdded = false;
+      $scope.searchBox = {show:false};
+      $scope.searchContact = {"searchtext" :""};
       $scope.initialize = function() {
-          $scope.getMyFriends();
+          $scope.getMyFriends($scope.searchContact.searchtext);
       }; 
 
-      $scope.getMyFriends = function() {
-          FriendsService.getMyFriends().then(function(response){
+      $scope.showInviteFriendPopup = function(){
+          $scope.searchUserContact('',$scope.startIndex, $scope.endIndex);
+          $scope.modal.show();
+      };
+
+      $scope.closeInviteFriendPopup = function () {
+          $scope.modal.hide();
+          $scope.resetPopup();
+      };
+      $scope.resetPopup = function() {
+          $scope.endIndex = 9;
+          $scope.searchContact.searchtext =  "";
+      };
+      $scope.getMyFriends = function(searchText) {
+          FriendsService.getMyFriends(searchText).then(function(response){
               if(response != undefined && Array.isArray(response))   {
                 $scope.myFriends = response;
-                 $scope.noFriendAdded = $scope.myFriends.length == 0;
+                $scope.searchBox.show  = false;
+                //  $scope.noFriendAdded = $scope.myFriends.length == 0;
               }else{
                   $flaskUtil.alert("Failed to load friends List");
+                  $scope.searchBox.show  = false;
               }           
           });
+      };
+      $scope.getAllfilteredFrieds = function(searchText) {
+          $scope.getMyFriends (searchText);
+      };
+
+      $scope.filterUserContact = function(searchText) {
+          $scope.endIndex = 9;
+          $scope.searchUserContact(searchText,$scope.startIndex,$scope.endIndex);
+      };
+
+      $scope.getMoreUserContact = function(searchText)  {
+          $scope.endIndex +=10;
+          $scope.searchUserContact(searchText, $scope.startIndex, $scope.endIndex); 
       }
+      $scope.searchUserContact = function(searchText, startIndex, endIndex) {
+           FriendsService.searchUserContact( $scope.searchContact.searchtext, $scope.startIndex, $scope.endIndex).then(function(response){
+                if(response != undefined && Array.isArray(response))   {
+                        $scope.userContactList = response;
+              }else{
+                  $flaskUtil.alert("Failed to load contact List");
+              }   
+           });
+      };
+        $scope.unBlockUser = function(userObject) {
+            FriendsService.unBlockUser(userObject.userId).then(function(response){
+                if(response != undefined)   {
+                    userObject.block = false;
+              }else{
+                  $flaskUtil.alert("Failed to unblock user");
+              }   
+           });
+        };
+        $scope.sendFriendRequest = function(userObject) {
+            FriendsService.sendFriendRequest(userObject.userId).then(function(response){
+                if(response != undefined)   {
+                    userObject.connectionRequested = true;
+              }else{
+                  $flaskUtil.alert("Failed to unblock user");
+              }   
+           });
+        };
       $ionicModal.fromTemplateUrl('templates/modal.html', {
             scope: $scope
         }).then(function (modal) {
