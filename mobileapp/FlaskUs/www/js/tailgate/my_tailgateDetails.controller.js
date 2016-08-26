548a8b65c6d7362a12e28ff0168c3c760d7f4824
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-26 16:39:47 +0000

    my tailgate details controller
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1876 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ mobileapp/FlaskUs/www/js/tailgate/my_tailgateDetails.controller.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+    angular.module('flaskApp')
+        .controller('mytailgateDetailsCtrl', mytailgateDetailsCtrl);
+
+    mytailgateDetailsCtrl.$inject = ['$scope', '$state', 'SERVER', '$stateParams', 'TailgateService'];
+
+    /* @ngInject */
+    function mytailgateDetailsCtrl($scope, $state, SERVER, $stateParams, TailgateService) {
+        $scope.myTailgaters = [];
+        var tailGateId = $stateParams.tailgateId;
+        $scope.imgUrl = SERVER.hostName + "c/document_library/get_file?uuid=";
+        getMyTailgate();
+        function getMyTailgate() {
+            TailgateService.getTailgate(tailGateId).then(function (respData) {
+                console.log(respData);
+                $scope.myTailgate = respData.data;
+                getlocationName($scope.myTailgate.eventId);
+                getMyTailgateImages(tailGateId);
+                getTailgaters(tailGateId);
+            });
+        }
+        function getlocationName(evntId) {
+            TailgateService.getEvent(evntId).then(function (respData) {
+                console.log(respData);
+                $scope.myeventLocation = respData.data;
+            });
+        }
+        function getMyTailgateImages(tailGateId) {
+            TailgateService.getMyTailgateImages(tailGateId).then(function (respData) {
+                console.log(respData);
+                $scope.myTailgateImages = respData.data;
+            });
+        }
+        function getTailgaters(tailGateId) {
+            TailgateService.getMyTailgateUsers(tailGateId).then(function (respData) {
+                console.log(respData);
+                $scope.myTailgaters = respData.data;
+            });
+        }
+    }
+})();
+
