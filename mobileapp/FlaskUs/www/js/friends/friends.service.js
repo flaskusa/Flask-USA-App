410379eb7c0c819608daf9d35e7e8701d6482687
rajesh <rajesh@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-29 06:48:14 +0000

    removed ionic loader
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1892 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/friends/friends.service.js
+++ mobileapp/FlaskUs/www/js/friends/friends.service.js
@@ -5,9 +5,9 @@
         .module('flaskApp')
         .service('FriendsService', FriendsService);
 
-    FriendsService.$inject = ['$http','SERVER','$ionicLoading','$q'];
+    FriendsService.$inject = ['$http','SERVER','$q'];
 
-    function FriendsService($http, SERVER,$ionicLoading,$q) {
+    function FriendsService($http, SERVER,$q) {
         var services =  {
             getMyFriends :getMyFriends,
             getFriendByUserId :getFriendByUserId ,
@@ -18,7 +18,6 @@
             sendFriendRequest : sendFriendRequest,
             unBlockUser : unBlockUser
         }
-        var spinnerLoading = false;
         var url = SERVER.url;
         var searchMyFriend = "/flask-social-portlet.entry/search-my-friends";
         var searchFriendById = "/flask-social-portlet.entry/get-user-by-id"
@@ -30,29 +29,18 @@
         var unBlockUserPath = "/flask-social-portlet.entry/unblock-user";
         var companyId = SERVER.companyId;
          function getMyFriends(searchText) {
-             if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                     return $http.get(url+searchMyFriend,{params:{
                         companyId: companyId,
                         keywords: searchText
                     }})
                     .then(function success(response) {
-                        $ionicLoading.hide();
-                        spinnerLoading = false;
                         return response.data;
                     }, function failure(response) {
-                        $ionicLoading.hide();
-                        spinnerLoading = false;
                         return $q.$inject(response);
                         //add errror handling 
                     });
-             }
         }
         function searchUserContact(keyword, start, end) {
-             if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                     return $http.get(url+searchUserContactPath,{params:{
                         companyId: companyId,
                         keywords: keyword,
@@ -60,134 +48,81 @@
                         end:end
                     }})
                     .then(function success(response) {
-                        $ionicLoading.hide();
-                        spinnerLoading = false;
                         return response.data;
                     }, function failure(response) {
-                        $ionicLoading.hide();
-                        spinnerLoading = false;
                         return $q.$inject(response);
                         //add errror handling 
                     });
-             }
         }
         function getFriendByUserId(userId) {
-            if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+searchFriendById,{params:{
                     userId: userId
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
 
         function sendMessage (userId,messgae,sendEmail) {
-            if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+sendFlaskMessage,{params:{
                     recipients: userId,
                     message:messgae,
                     sendEmail:sendEmail
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
         function blockUser(blockUserId) {
-              if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+blockUserPath,{params:{
                     blockUserId: blockUserId
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
         function unFriend(receiverUserId) {
-             if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+unFriendPath,{params:{
                     receiverUserId: receiverUserId
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
 
         function sendFriendRequest (receiverUserId) {
-            if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+addFriendsPath,{params:{
                     receiverUserId: receiverUserId
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
         function unBlockUser (unblockUserId) {
-            if(!spinnerLoading) {
-                $ionicLoading.show();
-                spinnerLoading = true;
                 return $http.get(url+unBlockUserPath,{params:{
                     unblockUserId: unblockUserId
                 }})
                 .then(function success(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return response.data;
                 }, function failure(response) {
-                    $ionicLoading.hide();
-                    spinnerLoading = false;
                     return $q.$inject(response);
                     //add errror handling 
                 });
-            }
         }
 
 return services;
