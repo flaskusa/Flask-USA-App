4fa09d7be4d5d42c282111e4c50007528d4b3b11
arai <arai@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-29 03:02:28 +0000

    added empty javascript sample map
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1888 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/events/event_map_view.js
+++ mobileapp/FlaskUs/www/js/events/event_map_view.js
@@ -3,15 +3,42 @@
     angular.module('flaskApp')
             .controller('eventMapViewCtrl', eventMapViewCtrl);
 
-    eventMapViewCtrl.$inject = ['$scope', '$stateParams', '$state','EventsService'];
+    eventMapViewCtrl.$inject = ['$scope', '$stateParams', '$state', '$ionicPlatform', 'EventsService'];
 
     /* @ngInject */
-    function eventMapViewCtrl($scope, $stateParams, $state, EventsService) {
+    function eventMapViewCtrl($scope, $stateParams, $state, $ionicPlatform, EventsService) {
         /* jshint validthis: true */
         var self = this;
-        var eventDetails = angular.toJson($stateParams.eventDetails.Event);
-        var eventObj = angular.fromJson($stateParams.eventDetails.Event);
-        $scope.eventName = eventObj.eventName;
-        console.log(JSON.stringify($stateParams));
+
+        
+
+        initEventData()
+        initMap();
+
+        function initEventData(){
+            try{
+            var eventDetails = angular.toJson($stateParams.eventDetails.Event);
+            var eventObj = angular.fromJson($stateParams.eventDetails.Event);
+            $scope.eventName = eventObj.eventName;
+            }catch(ex){
+
+            }
+        }
+
+        function initMap(){
+          // Getting the map selector in DOM
+            var mapDiv = document.getElementById("map_canvas");
+            var latLng = new google.maps.LatLng(42.3314, -83.0458);
+            
+            var mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+        
+            $scope.map = new google.maps.Map(mapDiv, mapOptions);
+
+        }
+        
     }
 })(); 
