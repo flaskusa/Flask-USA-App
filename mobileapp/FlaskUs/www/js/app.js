acaffa7af472eef7847573a69d9be1559ade982c
rajesh <rajesh@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-30 08:03:56 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1913 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/app.js
+++ mobileapp/FlaskUs/www/js/app.js
@@ -4,10 +4,10 @@
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
-             if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
-                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                 cordova.plugins.Keyboard.disableScroll(true);
-             }
+            //  if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
+            //      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            //      cordova.plugins.Keyboard.disableScroll(true);
+            //  }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
@@ -21,6 +21,7 @@
                 $ionicLoading.hide()
             })
              $rootScope.$on('catchAll:exception', function (event, data) { 
+                 $ionicLoading.hide()
                  // if exception occurs the  redirect ro events page
                 $state.go("app.events");
             })
---
+++ mobileapp/FlaskUs/www/js/friendsGroup/friends.group.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    angular.module('flaskApp')
+        .controller('FriendsGroupCtrl', FriendsGroupCtrl);
+
+    FriendsGroupCtrl.$inject = [];
+
+    /* @ngInject */
+    function FriendsGroupCtrl() {
+        // $scope.initialize = function() {
+        // }; 
+        //     $scope.initialize();
+       }
+
+})();
