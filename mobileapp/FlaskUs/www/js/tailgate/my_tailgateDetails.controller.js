bb12be95f77fa74d9e912a046a609f137f252245
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-08-31 18:32:47 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@1937 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/tailgate/my_tailgateDetails.controller.js
+++ mobileapp/FlaskUs/www/js/tailgate/my_tailgateDetails.controller.js
@@ -3,15 +3,33 @@
     angular.module('flaskApp')
         .controller('mytailgateDetailsCtrl', mytailgateDetailsCtrl);
 
-    mytailgateDetailsCtrl.$inject = ['$scope', '$state', 'SERVER', '$stateParams', 'TailgateService'];
+    mytailgateDetailsCtrl.$inject = ['$scope', '$state', 'SERVER', '$stateParams', 'TailgateService' ,'$cookies'];
 
     /* @ngInject */
-    function mytailgateDetailsCtrl($scope, $state, SERVER, $stateParams, TailgateService) {
+    function mytailgateDetailsCtrl($scope, $state, SERVER, $stateParams, TailgateService, $cookies) {
         $scope.myTailgaters = [];
         var tailGateId = $stateParams.tailgateId;
+        $cookies.put('tailGateId', tailGateId);
         $scope.imgUrl = SERVER.hostName + "c/document_library/get_file?uuid=";
-       // initMap();
         getMyTailgate();
+        $scope.goBack=function() {
+            $state.go("app.my_tailgate");
+        }
+        function initMap() {
+            // Getting the map selector in DOM
+            var mapDiv = document.getElementById("map_canvas");
+            var latLng = new google.maps.LatLng(42.3314, -83.0458);
+
+            var mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            $scope.map = new google.maps.Map(mapDiv, mapOptions);
+
+        }
+
         function getMyTailgate() {
             TailgateService.getTailgate(tailGateId).then(function (respData) {
                 console.log(respData);
