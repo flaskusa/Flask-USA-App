fd1ee276734d93c8dd488ebbf59ecb441ec87166
savitha <savitha@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-09-08 05:56:04 +0000

    Navnath Changed controller for add_my_tailgate
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@2024 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/tailgate/add_my_tailgate.controller.js
+++ mobileapp/FlaskUs/www/js/tailgate/add_my_tailgate.controller.js
@@ -3,25 +3,65 @@
     angular.module('flaskApp')
         .controller('add_mytailgateCtrl', add_mytailgateCtrl);
 
-    add_mytailgateCtrl.$inject = ['$scope', 'TailgateService', '$cordovaDatePicker', '$timeout', '$ionicSlideBoxDelegate', '$ionicScrollDelegate'];
+    add_mytailgateCtrl.$inject = ['$scope', 'TailgateService', '$cordovaDatePicker', '$timeout', '$ionicSlideBoxDelegate', '$ionicScrollDelegate', '$filter'];
 
-    /* @ngInject */
-    function add_mytailgateCtrl($scope, TailgateService, $cordovaDatePicker, $timeout, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
+    / @ngInject /
+    function add_mytailgateCtrl($scope, TailgateService, $cordovaDatePicker, $timeout, $ionicSlideBoxDelegate, $ionicScrollDelegate, $filter) {
         //for adding tailgate
-        $ionicSlideBoxDelegate.update();
-        $ionicScrollDelegate.resize()
         var self = this;
+        $scope.CurrEvent = [];
+        $scope.addTailgateParams = {
+            tailgateName: '',
+            tailgateDescription: '',
+            eventId: '',
+            eventName: '',
+            venmoAccountId: '',
+            amountToPay: ''
+        }
 
         $scope.pageFlow = {
-            disableOtherTabs : true
+            disableOtherTabs: true
         }
 
         $scope.pageFlow.disableOtherTabs = false;
 
         getallEventnames();
+
+        $scope.LoadSessionData = function () {
+            var EventId = 0;
+            var shownVal = document.getElementById("envtName").value;
+            var EventCol = document.querySelector("#EventNameList option[value='" + shownVal + "']");
+            if (EventCol == null) {
+                EventId = 0;
+            } else {
+                EventId = EventCol.getAttribute("data_Id");
+            }
+            $scope.addTailgateParams.eventId = EventId;
+            getEventDetails(EventId);
+        }
+        function getEventDetails(eventId) {
+            TailgateService.getEvent(eventId).then(function (respData) {
+                console.log(respData.data);
+                $scope.CurrEvent = respData.data;
+                var currStartTime = new Date($filter('date')($scope.CurrEvent.startTime, "MM-dd-yyyy HH:mm"));
+                var currEndTime = new Date($filter('date')($scope.CurrEvent.endTime, "MM-dd-yyyy HH:mm"));
+                var currDate = new Date($filter('date')($scope.CurrEvent.eventDate, "MM-dd-yyyy HH:mm"));
+                $scope.addTailgateParams.startTime = currStartTime;
+                $scope.addTailgateParams.endTime = currEndTime;
+                $scope.addTailgateParams.tailgateDate = currDate;
+                $scope.isavail = true;
+            });
+
+        }
+        $scope.isavail = false;
         $scope.addmyTailgate = function (tailgatedata) {
             console.log(tailgatedata);
+            tailgatedata.tailgateDate = new Date(tailgatedata.tailgateDate).getTime();
+            tailgatedata.endTime = new Date(tailgatedata.endTime).getTime();
+            tailgatedata.startTime = new Date(tailgatedata.startTime).getTime();
+            console.log($scope.tailgatedata);
             TailgateService.addTailgate(tailgatedata).then(function (respData) {
+                console.log(respData.data);
                 console.log(respData.data.exception);
             });
         }
@@ -40,14 +80,7 @@
             TailgateService.getallEvents().then(function (respData) {
                 console.log(respData.data.Events);
                 $scope.eventDetails = respData.data.Events;
-                var len = $scope.eventDetails.length - 1;
-                for (var i = 0; i <= len; i++) {
-                    if ($scope.eventNames.indexOf($scope.eventDetails[i].eventName) == -1) {
-                        $scope.eventNames.push({ 'eventName': $scope.eventDetails[i].eventName, 'eventId': $scope.eventDetails[i].eventId })
-                    }
-                }
-                console.log($scope.eventNames);
-            });            
+            });
         }
 
         $scope.slideHasChanged = function ($index) {
@@ -56,4 +89,3 @@
         };
     }
 })();
-
