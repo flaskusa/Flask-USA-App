6b365a92e39726c6175c9fd8a2d59c95e4261573
savitha <savitha@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-09-22 12:15:53 +0000

    update user for account settings page
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@2172 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/register_user/user.service.js
+++ mobileapp/FlaskUs/www/js/register_user/user.service.js
@@ -10,8 +10,17 @@
         var baseURL = SERVER.url;
         var addUserURL = "/flask-rest-users-portlet.flaskadmin/sign-up";
         var getUserByEmailId = "/flask-rest-users-portlet.flaskadmin/get-user-for-email";
+        var updateUserURL = "flask-rest-users-portlet.flaskadmin/update-flask-user";
+        var getUserByIdURL="flask-rest-users-portlet.flaskadmin/get-user-by-id";
         
-        this.saveUser = saveUser
+
+        var userServices = {
+            saveUser: saveUser,
+            getUserbyEmail: getUserbyEmail,
+            getUserById: getUserById,
+            updateUser: updateUser
+        }
+
         function saveUser(user, gender, srcname) {
             
             console.log(user);
@@ -39,7 +48,7 @@
                 });
         }
 
-        this.getUserbyEmail = function (email) {
+        function getUserbyEmail(email) {
             return $http.get(baseURL + getUserByEmailId, { params: { 'emailAddress': email.Email } })
                 .then(function success(response) {
                     return response;
@@ -47,6 +56,46 @@
                     console.log("failed");
                 });
         }
+
+        function getUserById(userId) {
+            return $http.get(baseURL + getUserByIdURL, {
+                params: {
+                    'userId': userId
+                }
+            })
+               .then(function success(response) {
+                   return response.data;
+               }, function failure(response) {
+                   console.log("failed");
+               });
+        }
+
+        function updateUser(user, gender, srcname) {
+            var params = {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.Email,
+                screenName: srcname,
+                password1: user.password1,
+                password2: user.password2,
+                DOB: user.DOB,
+                isMale: gender,
+                areaCode: user.areaCode,
+                mobileNumber: user.mobileNumber
+            };
+            return $http.get(baseURL + addUserURL, {
+                params
+            }
+            )
+            .then(function success(response) {
+                return response;
+            }, function failure(response) {
+                //add errror handling 
+                console.log("failed");
+            });
+        }
+
+        return userServices;
     }
 })();
 
