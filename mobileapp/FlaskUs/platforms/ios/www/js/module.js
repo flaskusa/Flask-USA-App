3fa617474ea908a94eec0906c059b4f8f3e9a10a
arai <arai@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2016-11-29 12:28:21 +0000

    git-svn-id: svn://127.0.0.1/flaskdump/trunk@2593 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/platforms/ios/www/js/module.js
+++
@@ -1,4 +0,0 @@
-﻿(function () {
-    'use strict';
-    angular.module('flaskApp', ['ionic', 'flaskApp.controllers', 'ngCookies', 'ngCordova', 'ngStorage', 'uiGmapgoogle-maps', 'nemLogging', 'ionic-datepicker','ionic-timepicker','ionic.closePopup']);
-})();
