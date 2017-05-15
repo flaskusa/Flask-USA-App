5768ca326cb8835683cd1385df45bdabafc7c970
shalaka.joshi <shalaka.joshi@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2017-05-15 16:47:28 +0000

    Added some remaining files and one finder method in service.xml
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@2959 386f392f-2c41-1343-9bf7-4b6fedb51b34

---
+++ liferay-plugins-sdk-6.2/portlets/flask-social-portlet/docroot/WEB-INF/src/com/liferay/contacts/service/impl/FlaskGroupMessagesServiceImpl.java
@@ -0,0 +1,200 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contacts.service.impl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.liferay.contacts.model.FlaskGroupMessages;
+import com.liferay.contacts.model.FlaskGroupRecipients;
+import com.liferay.contacts.model.impl.FlaskGroupRecipientsImpl;
+import com.liferay.contacts.service.FlaskGroupMessagesLocalServiceUtil;
+import com.liferay.contacts.service.FlaskGroupRecipientsLocalServiceUtil;
+import com.liferay.contacts.service.FlaskGroupRecipientsServiceUtil;
+import com.liferay.contacts.service.FlaskRecipientsLocalServiceUtil;
+import com.liferay.contacts.service.base.FlaskGroupMessagesServiceBaseImpl;
+import com.liferay.contacts.service.persistence.FlaskGroupMessagesUtil;
+import com.liferay.contacts.service.persistence.FlaskGroupRecipientsUtil;
+import com.liferay.contacts.service.persistence.FlaskRecipientsUtil;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+/**
+ * The implementation of the flask group messages remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.contacts.service.FlaskGroupMessagesService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see com.liferay.contacts.service.base.FlaskGroupMessagesServiceBaseImpl
+ * @see com.liferay.contacts.service.FlaskGroupMessagesServiceUtil
+ */
+public class FlaskGroupMessagesServiceImpl
+	extends FlaskGroupMessagesServiceBaseImpl {
+	
+	@Override
+	 public FlaskGroupMessages sendFlaskGroupMessage(long groupId, String message, boolean sendEmail, ServiceContext serviceContext){
+	  FlaskGroupMessages flaskGroupMessage = null;
+	  try {
+		   User user = UserLocalServiceUtil.getUserById(serviceContext.getUserId());
+		   flaskGroupMessage = FlaskGroupMessagesLocalServiceUtil.createFlaskGroupMessages(CounterLocalServiceUtil.increment());
+		   flaskGroupMessage.setSenderEmail(user.getEmailAddress());
+		   flaskGroupMessage.setSenderName(user.getFullName());
+		   flaskGroupMessage.setGroupId(groupId);
+		   flaskGroupMessage.setSenderUserId(user.getUserId());
+		   flaskGroupMessage.setMessage(message);
+		   flaskGroupMessage.setSendEmail(sendEmail);
+		   Date date = new Date();
+		   flaskGroupMessage.setDateTime(serviceContext.getCreateDate(date));
+		   flaskGroupMessage = FlaskGroupMessagesLocalServiceUtil.addFlaskGroupMessages(flaskGroupMessage);
+		   FlaskGroupRecipientsServiceUtil.addFlaskGroupRecipients(groupId, user.getEmailAddress(), flaskGroupMessage.getGroupMessagesId(), flaskGroupMessage.getMessage(), flaskGroupMessage.getSendEmail(), serviceContext);
+	   } catch (Exception e) {
+	   e.printStackTrace();
+	  }
+	  return flaskGroupMessage;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONArray getAllMyFlaskGroupMessages(long groupId, ServiceContext serviceContext){
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+		try {
+			DynamicQuery groupQuery = DynamicQueryFactoryUtil.forClass(FlaskGroupRecipientsImpl.class);
+			groupQuery.add(PropertyFactoryUtil.forName("groupId").eq(new Long(groupId)));
+			groupQuery.addOrder(OrderFactoryUtil.desc("receivedDateTime"));
+			groupQuery.setLimit(0, 100);
+			List<FlaskGroupRecipients> flaskGroupRecipients = FlaskRecipientsLocalServiceUtil.dynamicQuery(groupQuery);
+			for(FlaskGroupRecipients recp: flaskGroupRecipients){
+				JSONObject jsonObj =  JSONFactoryUtil.createJSONObject();
+				FlaskGroupMessages msg = FlaskGroupMessagesLocalServiceUtil.getFlaskGroupMessages(recp.getGroupMessageId());
+				jsonObj.put("dateTime", msg.getDateTime());
+				jsonObj.put("message", msg.getMessage());
+				jsonObj.put("messageId", msg.getGroupMessagesId());
+				jsonObj.put("recipients", recp.getRecipients());
+				jsonObj.put("read", recp.getRead());
+				jsonObj.put("sendEmail", msg.getSendEmail());
+				jsonObj.put("senderEmail", msg.getSenderEmail());
+				jsonObj.put("senderName", msg.getSenderName());
+				jsonObj.put("senderUserId", msg.getSenderUserId());
+				jsonObj.put("portraitId", UserLocalServiceUtil.getUserById(msg.getSenderUserId()).getPortraitId());
+				jsonArray.put(jsonObj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jsonArray;
+	}
+	
+	@Override
+	public List<FlaskGroupMessages> getGroupMessages(long groupId){
+		List<FlaskGroupMessages> flaskGroupMessages = new ArrayList<FlaskGroupMessages>();
+		try {
+			flaskGroupMessages = FlaskGroupMessagesUtil.findBygroupId(groupId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flaskGroupMessages;
+	}
+	
+	@Override
+	public int getMyFlaskGroupMessagesCount(long groupId, ServiceContext serviceContext){
+		int count = 0;
+		try {
+			List<FlaskGroupRecipients> flaskGroupRecipients = FlaskGroupRecipientsUtil.findByGroupId(groupId);
+			for(@SuppressWarnings("unused") FlaskGroupRecipients recp: flaskGroupRecipients){
+				count++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+	@Override
+	public int getMyUnreadFlaskGroupMessagesCount(long groupId, ServiceContext serviceContext){
+		int count = 0;
+		try {
+			List<FlaskGroupRecipients> flaskGroupRecipients = FlaskGroupRecipientsUtil.findByGroupId(groupId);
+			for(FlaskGroupRecipients recp: flaskGroupRecipients){
+				boolean flag = false;
+				String recipient = recp.getRecipients();
+				String read = recp.getRead();
+				String[] recpPart = recipient.split(",");
+				String[] readPart = read.split(","); 
+				for(int i = 0; i <= recpPart.length-1; i++){
+					if((serviceContext.getUserId() == Long.parseLong(recpPart[i])) && (Long.parseLong(readPart[i]) == 0)){
+						flag = true;
+					}
+				}
+				if(flag){
+					count++;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+	@Override
+	public void deleteGroupMessage(long groupMessageId, ServiceContext serviceContext){
+		try {
+			List<FlaskGroupRecipients> recipients =  FlaskGroupRecipientsUtil.findByMessageId(groupMessageId);
+			FlaskGroupRecipientsLocalServiceUtil.deleteFlaskGroupRecipients(recipients.get(0));
+			int recCount = FlaskRecipientsUtil.countBymessageId(groupMessageId);
+			if(recCount <= 0){
+				FlaskGroupMessages msg = FlaskGroupMessagesLocalServiceUtil.getFlaskGroupMessages(groupMessageId);
+				FlaskGroupMessagesLocalServiceUtil.deleteFlaskGroupMessages(msg);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void deleteGroupMessagesByDateRange(Date startDate, Date endDate, ServiceContext serviceContext){
+		try {
+			List<FlaskGroupMessages> messages =  FlaskGroupMessagesLocalServiceUtil.getFlaskGroupMessageses(0, FlaskGroupMessagesLocalServiceUtil.getFlaskGroupMessagesesCount());
+			for(FlaskGroupMessages msg: messages){
+				Date msgDate = new Date(msg.getDateTime().getDate());
+				Date stDate = new Date(startDate.getDate());
+				Date enDate = new Date(endDate.getDate());
+				if(stDate.compareTo(msgDate) <= 0 && enDate.compareTo(msgDate) >= 0){
+					List<FlaskGroupRecipients> recipients = new ArrayList<FlaskGroupRecipients>();
+					recipients =  FlaskGroupRecipientsUtil.findByMessageId(msg.getGroupMessagesId());
+					FlaskGroupRecipientsLocalServiceUtil.deleteFlaskGroupRecipients(recipients.get(0));
+					FlaskGroupMessagesLocalServiceUtil.deleteFlaskGroupMessages(msg);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ liferay-plugins-sdk-6.2/portlets/flask-social-portlet/docroot/WEB-INF/src/com/liferay/contacts/service/impl/FlaskGroupRecipientsServiceImpl.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contacts.service.impl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.liferay.contacts.model.FlaskGroupRecipients;
+import com.liferay.contacts.model.impl.FlaskGroupRecipientsImpl;
+import com.liferay.contacts.service.FlaskGroupRecipientsLocalServiceUtil;
+import com.liferay.contacts.service.FlaskRecipientsLocalServiceUtil;
+import com.liferay.contacts.service.base.FlaskGroupRecipientsServiceBaseImpl;
+import com.liferay.contacts.service.persistence.FlaskGroupRecipientsUtil;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.rumbasolutions.flask.email.util.EmailInvitationUtil;
+import com.rumbasolutions.flask.model.FlaskGroupUsers;
+import com.rumbasolutions.flask.service.FlaskGroupUsersServiceUtil;
+
+/**
+ * The implementation of the flask group recipients remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.contacts.service.FlaskGroupRecipientsService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see com.liferay.contacts.service.base.FlaskGroupRecipientsServiceBaseImpl
+ * @see com.liferay.contacts.service.FlaskGroupRecipientsServiceUtil
+ */
+public class FlaskGroupRecipientsServiceImpl
+	extends FlaskGroupRecipientsServiceBaseImpl {
+	
+	private static Log LOGGER = LogFactoryUtil
+			.getLog(FlaskGroupRecipientsServiceImpl.class);
+	
+	@Override
+	public FlaskGroupRecipients addFlaskGroupRecipients(long groupId, String senderEmailId, long groupMessageId, String message, Boolean sendEmail, ServiceContext serviceContext){
+		FlaskGroupRecipients flaskGroupRecipients = null;
+		try {
+			flaskGroupRecipients = FlaskGroupRecipientsLocalServiceUtil.createFlaskGroupRecipients(CounterLocalServiceUtil.increment());
+			List<FlaskGroupUsers> groupUsers= FlaskGroupUsersServiceUtil.getAllGroupUsers(groupId);
+			String recipients ="";
+			String read ="";
+			   for (FlaskGroupUsers groupUser : groupUsers){
+				   if((recipients == null || recipients == "") && (read == null || read == "")){
+					   recipients = String.valueOf(groupUser.getUserId());
+					   read = "0";
+				   }else{
+					   recipients = recipients+ "," +groupUser.getUserId();
+					   read = read+ "," + "0";
+				   }
+				   EmailInvitationUtil.emailMessage(groupUser.getUserName(), senderEmailId, groupUser.getEmailAddress(), message, serviceContext);
+			   }
+			flaskGroupRecipients.setRecipients(recipients);
+			flaskGroupRecipients.setRead(read);
+			flaskGroupRecipients.setGroupId(groupId);
+			flaskGroupRecipients.setGroupMessageId(groupMessageId);
+			flaskGroupRecipients.setRead(read);
+			flaskGroupRecipients.setSenderId(serviceContext.getUserId());
+			Date date = new Date();
+			flaskGroupRecipients.setReceivedDateTime(serviceContext.getCreateDate(date));
+			flaskGroupRecipients = FlaskGroupRecipientsLocalServiceUtil.addFlaskGroupRecipients(flaskGroupRecipients);
+			setGroupMessageRead(groupMessageId, serviceContext);
+		} catch (Exception e) {
+			LOGGER.error("Exception in addFlaskGroupRecipients:"+e.getMessage());
+		}
+		return flaskGroupRecipients;
+	}
+	
+	@Override
+	 public boolean setGroupMessageRead(long groupMessageId, ServiceContext serviceContext){
+	  boolean ret = false;
+	  try {
+		   List<FlaskGroupRecipients> flaskGroupRecipients = FlaskGroupRecipientsUtil.findByMessageId(groupMessageId);
+		   for(FlaskGroupRecipients recp: flaskGroupRecipients){
+			    String recipient = recp.getRecipients();
+			    String read = recp.getRead();
+				String[] recpPart = recipient.split(",");
+				String[] readPart = read.split(","); 
+				String updateRead ="";
+				for(int i = 0; i <= recpPart.length-1; i++){
+					if((serviceContext.getUserId() == Long.parseLong(recpPart[i])) && (Long.parseLong(readPart[i]) == 0)){
+						readPart[i] = "1"; 
+						ret = true;
+					}
+					if(updateRead == null || updateRead == ""){
+						updateRead = readPart[i];
+					}else{
+						updateRead = updateRead+ "," + readPart[i];
+					}
+				}
+				if(ret){
+					recp.setRead(updateRead);
+					FlaskGroupRecipientsLocalServiceUtil.updateFlaskGroupRecipients(recp);
+				}
+		   }
+	  } catch (Exception e) {
+		  LOGGER.error("Exception in setGroupMessageRead:"+e.getMessage());
+	  }
+	  return ret;
+	 }
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<FlaskGroupRecipients> getGroupRecipientsByGroupId(long groupId){
+		List<FlaskGroupRecipients> flaskGroupRecipients = new ArrayList<FlaskGroupRecipients>();
+		try {
+			DynamicQuery groupQuery = DynamicQueryFactoryUtil.forClass(FlaskGroupRecipientsImpl.class);
+			groupQuery.add(PropertyFactoryUtil.forName("groupId").eq(new Long(groupId)));
+			flaskGroupRecipients = FlaskRecipientsLocalServiceUtil.dynamicQuery(groupQuery);
+		} catch (Exception e) {
+			LOGGER.error("Exception in getGroupRecipientsByGroupId:"+e.getMessage());
+		}
+		return flaskGroupRecipients;
+	}
+}
