dea1e1b75f4e79c61dc0bf5a4b6ccca87f0494e0
navnath <navnath@386f392f-2c41-1343-9bf7-4b6fedb51b34>
2017-05-22 13:14:54 +0000

    added edit list on view of tailgate plan
    git-svn-id: svn://127.0.0.1/flaskdump/trunk@2972 386f392f-2c41-1343-9bf7-4b6fedb51b34

--- mobileapp/FlaskUs/www/js/tailgate/my_tailgate_plan.controller.js
+++ mobileapp/FlaskUs/www/js/tailgate/my_tailgate_plan.controller.js
@@ -3,10 +3,10 @@
     angular.module('flaskApp')
         .controller('mytailgatePlanCtrl', mytailgatePlanCtrl);
 
-    mytailgatePlanCtrl.$inject = ['$scope', '$state', '$stateParams', 'TailgateService', '$cookies', '$ionicModal'];
+    mytailgatePlanCtrl.$inject = ['$scope', '$state', '$stateParams', 'TailgateService', '$cookies', '$ionicModal', '$ionicLoading', '$ionicPopup'];
 
     /* @ngInject */
-    function mytailgatePlanCtrl($scope, $state, $stateParams, TailgateService, $cookies, $ionicModal) {
+    function mytailgatePlanCtrl($scope, $state, $stateParams, TailgateService, $cookies, $ionicModal, $ionicLoading, $ionicPopup) {
         $scope.myTailgaters;
         var supplyItemName;
         $scope.tailgateItems = [];
@@ -123,6 +123,129 @@
             window.open(paymentUrl, '_system', 'location=yes'); // for inapp browser or system app
         };
 
+        //for editing tailgate on view screen
+        $scope.selectedSupplyListItems = [];
+        $scope.showEditForm = function () {
+            $("#editSupplyListItems").show();
+            $("#showSupplyListItems").hide()
+        }
+
+        $scope.taigateSupplyList = [];
+        var tailgateId = $cookies.get("currtailGateId");
+        $scope.copytTailgateId = tailgateId;
+        var userDetail = $cookies.getObject('CurrentUser');
+        $scope.loggedInUserId = userDetail.data.userId;
+        $scope.curritemAssignedUserName = userDetail.data.firstName + " " + userDetail.data.lastName;
+        $scope.curritemAssignedUserId = JSON.stringify(userDetail.data.itemAssignedUserId);
+        $scope.currusercompanyId = userDetail.data.companyId;
+        var itemArray;
+
+        $scope.selectGameDaySupply = function (list, checked) {
+            if (checked == true) {
+                $scope.collapsedItems = false;
+                $scope.taigateSupplyList = list;
+                $scope.copyForMyGameDaySupply(list);
+                $scope.addNewTailgateSuppliesItem = true;
+            } else {
+                $scope.removeSelectedSupply(list);
+            }
+        }
+        /*Save supply Item in selected supply List */
+        $scope.saveTailgateSupplyItem = function (listItemName) {
+            if (listItemName != undefined && listItemName != "") {
+                if ($scope.taigateSupplyList.supplyListId > 0) {
+                    TailgateService.addSupplyItem(listItemName, $scope.taigateSupplyList.supplyListId).then(function (response) {
+                        $scope.allMyTailgateItems.push({
+                            supplyListItemName: response.supplyListItemName,
+                            tailgateSupplyItemId: response.tailgateSupplyItemId
+                        });
+                        $scope.addNewSuppliesItem = false;
+                    });
+                }
+                else {
+                    TailgateService.addTailgateSupplyItem(listItemName, tailgateId, $scope.loggedInUserId).then(function (response) {
+                        $scope.allMyTailgateItems.push({
+                            supplyListItemName: response.supplyListItemName,
+                            tailgateSupplyItemId: response.tailgateSupplyItemId
+                        });
+                    });
+                }
+                $ionicLoading.show({ template: listItemName + ' Suppy item added', noBackdrop: false, duration: 2000 });
+            }
+            else {
+                $ionicLoading.show({ template: 'Item name should not be empty', noBackdrop: false, duration: 2000 });
+            }
+        }
+        $scope.cancelTailgateSupplyItemAdding = function () {
+            $scope.addNewSuppliesItem = false;
+            $scope.listItemName = "";
+        }
+
+        $scope.updateSupplyItemsUser = function (data, index) {
+            TailgateService.updateTailgateSupplyItem(data.tailgateSupplyItemId, data.supplyListItemName, tailgateId, data.itemAssignedUserId).then(function (respData) {
+                $ionicLoading.show({ template: 'User changed successfully!', noBackdrop: false, duration: 3000 });
+            });
+        }
+
+        $scope.leaveSupplyItem = function (supplyItemId, index) {
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Delete Supply Item?'
+            });
+            confirmPopup.then(function (res) {
+                if (res) {
+                    TailgateService.deleteTailgateSupplyItem(supplyItemId).then(function (respData) {
+                        $scope.allMyTailgateItems.splice(index, 1);
+                    });
+
+                } else {
+                }
+            });
+        }
+
+        //Adding supply items to tailgate
+        $scope.addSupplyItems = function () {
+            var str = [];
+            var replaceStr;
+            $scope.items = [];
+            angular.forEach($scope.allMyTailgateItems, function (val, idx) {
+                var tempItemName;
+                tempItemName = val.supplyListItemName;
+                $scope.items.push(tempItemName);
+            })
+            for (var i = 0; i < $scope.items.length; i++) { 
+                    str.push($scope.items[i]);
+            }
+            itemArray = JSON.stringify(str);
+            // var tailgateId = $cookies.get("currtailGateId");
+            TailgateService.addTailgateSupplyItems(itemArray, tailgateId, "-1").then(function (respData) {
+                $scope.alltailgateSupplyItem = respData.data;
+                $scope.associateUserWithSupplyItem();
+            });
+
+        }
+
+        $scope.associateUserWithSupplyItem = function () {
+            angular.forEach($scope.alltailgateSupplyItem, function (value, index) {
+                console.log(value.itemAssignedUserId);
+                value.itemAssignedUserId = getAssigenUserId(decodeURIComponent(value.supplyListItemName));
+                $scope.updateSupplyItems(value);
+            })
+            $("#editSupplyListItems").hide();
+            $("#showSupplyListItems").show()
+            getTailgaters();
+        };
+
+        function getAssigenUserId(supplyItemName) {
+            var userId;
+            angular.forEach($scope.selectedSupplyListItems, function (value, idx) {
+                if (value.itemAssignedUserId > 0 && supplyItemName == value.supplyItemName) {
+                    userId = value.itemAssignedUserId;
+                    return;
+                    // $scope.updateSupplyItems(val);
+                }
+            })
+            return userId;
+        }
         getMyTailgate();
         // getAllMyTailgates();
         getTailgaters();
